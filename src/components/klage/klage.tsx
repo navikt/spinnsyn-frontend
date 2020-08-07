838a44a90245ca762e8a42fb54a61f668b77ab81
kulsveen <bjorn.inge.kulsveen@nav.no>
2020-08-07 15:38:34 +0200

    Enkel styling av klage

--- src/components/klage/klage.tsx
+++ src/components/klage/klage.tsx
@@ -2,7 +2,7 @@ import './klage.less'
 
 import { Knapp } from 'nav-frontend-knapper'
 import Lenke from 'nav-frontend-lenker'
-import { Element, Normaltekst } from 'nav-frontend-typografi'
+import { Normaltekst, Undertittel } from 'nav-frontend-typografi'
 import React from 'react'
 
 import { tekst } from '../../utils/tekster'
@@ -13,15 +13,15 @@ const Klage = () => {
     }
 
     return (
-        <>
-            <Element tag="h2" className="klage-tittel">{tekst('klage.tittel')}</Element>
-            <Normaltekst tag="span">{tekst('klage.behandlingstid')} </Normaltekst>
-            <Normaltekst tag="span">{tekst('klage.frist')} </Normaltekst>
+        <section className="vedtak__klage">
+            <Undertittel tag="h2">{tekst('klage.tittel')}</Undertittel>
+            <Normaltekst className="vedtak__klage--tekst">{tekst('klage.behandlingstid')} </Normaltekst>
+            <Normaltekst className="vedtak__klage--tekst">{tekst('klage.frist')} </Normaltekst>
             <Lenke target="blank" href={tekst('klage.klagerettigheter.url')}>
-                <Normaltekst tag="span">{tekst('klage.klagerettigheter.tekst')}</Normaltekst>
+                <Normaltekst className="vedtak__klage--tekst">{tekst('klage.klagerettigheter.tekst')}</Normaltekst>
             </Lenke>
             <Knapp mini type="standard" onClick={() => klageKnappTrykk()}>{tekst('klage.knapp.uenig')}</Knapp>
-        </>
+        </section>
     )
 }
 
