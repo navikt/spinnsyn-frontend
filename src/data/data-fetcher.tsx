4fa12da2714130b7a234700bbf18eb8e7f4e5e81
Kjell Arne Brødreskift <kjarbr@gmail.com>
2020-09-11 14:09:48 +0200

    Fjernet henting av søknader, sykmeldinger og inntektsmeldinger fra data-fetcher

--- src/data/data-fetcher.tsx
+++ src/data/data-fetcher.tsx
@@ -2,9 +2,6 @@ import Spinner from 'nav-frontend-spinner'
 import React, { useEffect } from 'react'
 
 import IngenData from '../pages/feil/ingen-data'
-import { Inntektsmelding } from '../types/inntektsmelding'
-import { RSSoknad } from '../types/rs-types/rs-soknad'
-import { Soknad, Sykmelding } from '../types/types'
 import { Vedtak } from '../types/vedtak'
 import env from '../utils/environment'
 import { logger } from '../utils/logger'
@@ -13,24 +10,10 @@ import { FetchState, hasAny401, hasAnyFailed, hasData, isAnyNotStartedOrPending,
 import { useAppStore } from './stores/app-store'
 
 export function DataFetcher(props: { children: any }) {
-    const { setSoknader, setSykmeldinger, setVedtak, setInntektsmeldinger } = useAppStore()
-    const rssoknader = useFetch<RSSoknad[]>()
-    const sykmeldinger = useFetch<Sykmelding[]>()
+    const { setVedtak } = useAppStore()
     const vedtak = useFetch<Vedtak[]>()
-    const inntektsmeldinger = useFetch<Inntektsmelding[]>()
 
     useEffect(() => {
-        if (isNotStarted(rssoknader)) {
-            rssoknader.fetch(env.syfoapiRoot + '/syfosoknad/api/soknader', {
-                credentials: 'include',
-            }, (fetchState: FetchState<RSSoknad[]>) => {
-                if (hasData(fetchState)) {
-                    setSoknader(fetchState.data!.map(soknad => {
-                        return new Soknad(soknad)
-                    }))
-                }
-            })
-        }
         if (isNotStarted(vedtak)) {
             vedtak.fetch(env.spinnsynRoot + '/api/v1/vedtak', {
                 credentials: 'include',
@@ -40,41 +23,22 @@ export function DataFetcher(props: { children: any }) {
                 }
             })
         }
-        if (isNotStarted(sykmeldinger)) {
-            sykmeldinger.fetch(env.sykmeldingerBackendProxyRoot + '/api/v1/syforest/sykmeldinger', {
-                credentials: 'include',
-            }, (fetchState: FetchState<Sykmelding[]>) => {
-                if (hasData(fetchState)) {
-                    setSykmeldinger(fetchState.data)
-                }
-            })
-        }
-        if (isNotStarted(inntektsmeldinger)) {
-            inntektsmeldinger.fetch(env.flexinntektsmeldingRoot + '/api/v1/inntektsmeldinger', {
-                credentials: 'include',
-            }, (fetchState: FetchState<Inntektsmelding[]>) => {
-                if (hasData(fetchState)) {
-                    setInntektsmeldinger(fetchState.data)
-                }
-            })
-        }
         // eslint-disable-next-line
-    }, [rssoknader]);
+    }, [vedtak]);
 
-    if (hasAny401([ rssoknader, sykmeldinger, vedtak ])) {
+    if (hasAny401([ vedtak ])) {
         window.location.href = hentLoginUrl()
 
-    } else if (isAnyNotStartedOrPending([ rssoknader, sykmeldinger, vedtak ])) {
+    } else if (isAnyNotStartedOrPending([ vedtak ])) {
         return <Spinner type={'XXL'} />
 
-    } else if (hasAnyFailed([ rssoknader, sykmeldinger, vedtak ])) {
-        logger.error('Klarer ikke hente en av disse [ rssoknader, sykmeldinger, vedtak ]')
+    } else if (hasAnyFailed([ vedtak ])) {
+        logger.error('Klarer ikke hente vedtak')
         return <IngenData />
     }
 
     return props.children
 }
-
 
 export const hentLoginUrl = () => {
     return `${env.loginServiceUrl}?redirect=${env.loginServiceRedirectUrl}`
