7cebfc6bc1f3ad918d1fbd748ebac06a97962a1f
Kjell Arne Brødreskift <kjarbr@gmail.com>
2020-09-18 14:31:31 +0200

    Test fix

--- src/components/banner/banner.test.ts
+++ src/components/banner/banner.test.ts
@@ -3,6 +3,6 @@ import 'dayjs/locale/nb'
 import { tekst } from '../../utils/tekster'
 
 it('Returns text from bundle', () => {
-    const text = tekst('spinnsyn.sidetittel')
+    const text = tekst('spinnsyn.sidetittel.liste')
     expect(text).toEqual('Utbetalinger')
 })
