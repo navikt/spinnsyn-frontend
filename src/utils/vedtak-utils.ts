efb9477f40628bb182486be948dc7dc38fb5de84
kulsveen <bjorn.inge.kulsveen@nav.no>
2020-09-23 13:26:51 +0200

    Tenkte feil, summerer opp totalen

--- src/utils/vedtak-utils.ts
+++ src/utils/vedtak-utils.ts
@@ -1,9 +1,7 @@
 import { Vedtak } from '../types/vedtak'
 
 export const refusjonTilArbeidsgiverBeløp = (vedtak: Vedtak) => {
-    const refusjonsUtbetalinger = vedtak.vedtak.utbetalinger
+    return vedtak.vedtak.utbetalinger
         .filter(v => v.fagområde === 'SPREF')
-        .flatMap(v => v.utbetalingslinjer)
-    return refusjonsUtbetalinger
-        .reduce((sum, betaling) => sum + betaling.beløp, 0)
+        .reduce((sum, betaling) => sum + betaling.totalbeløp, 0)
 }
