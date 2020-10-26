ef3489ef4e511e8656ee784df962bb30bbf8818e
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2020-10-26 14:58:36 +0100

    Skill på nye og tidligere tom tekst

--- src/pages/vedtak-liste/vedtak-liste-tekster.ts
+++ src/pages/vedtak-liste/vedtak-liste-tekster.ts
@@ -1,6 +1,7 @@
 const VedtakListeTekster = {
     'vedtak-liste.sidetittel': 'Utbetalinger',
-    'vedtak-liste.ingen-soknader': 'Du har ingen nye behandlede søknader fra NAV.',
+    'vedtak-liste.ingen-nye-soknader': 'Du har ingen nye behandlede søknader fra NAV.',
+    'vedtak-liste.ingen-tidligere-soknader': 'Du har ingen tidligere behandlede søknader fra NAV.',
     'vedtak-liste.sorter.etter': 'Sorter etter'
 }
 
