6a3b399bba0523f17d81beba476ddfeabedff0af
Kent Daleng <kent.stefan.daleng@nav.no>
2020-11-18 22:01:03 +0100

    legger til et komma

--- src/pages/vedtak-side/behandling/behandling-tekster.ts
+++ src/pages/vedtak-side/behandling/behandling-tekster.ts
@@ -5,7 +5,7 @@ const BehandlingTekster = {
     'behandling.lenke.url': 'https://mininnboks.nav.no/sporsmal/skriv/HELSE',
     'behandling.tekst2': ' om du ønsker å se opplysningene.',
     'behandling.preteritum.tekst1': 'Søknaden din ble behandlet automatisk. Opplysningene var hentet fra søknaden din, offentlige registre og inntektsmeldingen fra arbeidsgiveren din. ',
-    'behandling.preteritum.tekst2': 'Når søknaden blir behandlet på nytt vil dette ikke skje automatisk, men av en våre saksbehandlere.'
+    'behandling.preteritum.tekst2': 'Når søknaden blir behandlet på nytt, vil dette ikke skje automatisk, men av en våre saksbehandlere.'
 }
 
 export default BehandlingTekster
