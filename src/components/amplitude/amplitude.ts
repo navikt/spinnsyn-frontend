6acd9e7d1de07dee49322614ef47603ab1ffd12a
Kent Daleng <kent.daleng@nav.no>
2021-01-21 11:20:49 +0100

    ikke redefiner useragent

--- src/components/amplitude/amplitude.ts
+++ src/components/amplitude/amplitude.ts
@@ -39,7 +39,6 @@ export const [ AmplitudeProvider, useAmplitudeInstance ] = constate(() => {
                 },
             },
         )
-        instance.current._userAgent = ''
         // eslint-disable-next-line
     }, []);
 
