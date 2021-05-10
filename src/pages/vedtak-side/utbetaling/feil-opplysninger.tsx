808356e143cc47d82f9a7708aadf84481f905960
kulsveen <bjorn.inge.kulsveen@nav.no>
2021-05-10 14:57:57 +0200

    Tar tilbake utvidbar lenke

--- src/pages/vedtak-side/utbetaling/feil-opplysninger.tsx
+++ src/pages/vedtak-side/utbetaling/feil-opplysninger.tsx
@@ -1,8 +1,8 @@
-import Ekspanderbartpanel from 'nav-frontend-ekspanderbartpanel'
 import Lenke from 'nav-frontend-lenker'
 import { Element, Normaltekst } from 'nav-frontend-typografi'
 import React from 'react'
 
+import Utvidbar from '../../../components/utvidbar/utvidbar'
 import { useAppStore } from '../../../data/stores/app-store'
 import { tekst } from '../../../utils/tekster'
 import { klagefrist } from '../../../utils/vedtak-utils'
@@ -11,30 +11,26 @@ const FeilOpplysninger = () => {
     const { valgtVedtak } = useAppStore()
 
     return (
-        <Ekspanderbartpanel apen={false} className="tekstinfo" tittel={
-            <Element tag="span">Ved feil opplysninger</Element>
-        }>
-            <>
-                <Normaltekst className="tekstinfo__avsnitt">
-                    {tekst('utbetaling.opplysninger.inntektsmelding')}
-                </Normaltekst>
+        <Utvidbar erApen={false} tittel="Ved feil opplysninger" type="intern" className="tekstinfo">
+            <Normaltekst className="tekstinfo__avsnitt">
+                {tekst('utbetaling.opplysninger.inntektsmelding')}
+            </Normaltekst>
 
-                <Normaltekst className="tekstinfo__avsnitt">
-                    {tekst('uenig.tekst2')}
-                    <Lenke href={tekst('uenig.lenke1.url')} target="_blank">
-                        {tekst('uenig.lenke1')}
-                    </Lenke>,
-                    {tekst('uenig.tekst3')}
-                    <Lenke href={tekst('uenig.lenke2.url')} target="_blank">
-                        {tekst('uenig.lenke2')}
-                    </Lenke>.
-                </Normaltekst>
+            <Normaltekst className="tekstinfo__avsnitt">
+                {tekst('uenig.tekst2')}
+                <Lenke href={tekst('uenig.lenke1.url')} target="_blank">
+                    {tekst('uenig.lenke1')}
+                </Lenke>,
+                {tekst('uenig.tekst3')}
+                <Lenke href={tekst('uenig.lenke2.url')} target="_blank">
+                    {tekst('uenig.lenke2')}
+                </Lenke>.
+            </Normaltekst>
 
-                <Element className="tekstinfo__dobbel">
-                    {'Klagefrist: ' + klagefrist(valgtVedtak)}
-                </Element>
-            </>
-        </Ekspanderbartpanel>
+            <Element className="tekstinfo__dobbel">
+                {'Klagefrist: ' + klagefrist(valgtVedtak)}
+            </Element>
+        </Utvidbar>
     )
 }
 
