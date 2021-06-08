be4625acaf26fc6a5b6c9a758d171f01ce9bdb50
Kjell Arne Brødreskift <kjell.arne.brodreskift@nav.no>
2021-06-08 10:48:39 +0200

    Korrigert cypress-test

--- cypress/integration/utbetalingsoversikt.ts
+++ cypress/integration/utbetalingsoversikt.ts
@@ -21,17 +21,13 @@ describe('Tester visning av utbetalingsoversikt', () => {
         cy.contains('Daglig utbetalingsoversikt')
             .click({ force: true })
 
-        cy.contains('24.01.21').parent().should('contain', 'Arbeidsdag').and('contain', '-')
-        cy.contains('25.01.21').parent().should('contain', 'Arbeidsgiver betaler').and('contain', '-')
         cy.contains('08.02.21').parent().should('contain', 'Delvis syk').and('contain', '896')
         cy.contains('20.02.21').parent().should('contain', 'Helg').and('contain', '-')
     })
 
     it('Mer om dagtyper', () => {
-        cy.get('.tekstinfo .etikett').should('have.text', 'Arbeidsgiver betaler' + 'Arbeidsdag' + 'Delvis syk' + 'Helg')
+        cy.get('.tekstinfo .etikett').should('have.text', 'Delvis syk' + 'Helg')
         cy.get('.tekstinfo .typo-normal').should('have.text',
-            'Arbeidsgiveren din betaler de første 16 kalenderdagene av sykefraværet.' +
-            'Det utbetales ikke sykepenger for hele dager du har vært på jobben.' +
             'Du får sykepenger for den delen av arbeidstiden du ikke jobber. Vi bruker opplysningene dine om hvor mye du jobbet i perioden.' +
             'Sykepenger betales alltid fra mandag til fredag. Om du jobber lørdager og søndager blir dette medregnet i totalbeløpet.'
         )
