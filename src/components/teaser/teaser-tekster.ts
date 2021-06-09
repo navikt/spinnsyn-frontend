5250ee0570f50124f96c3c339867808d9436ff22
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2021-06-09 10:17:38 +0200

    Endre annullert tekster

--- src/components/teaser/teaser-tekster.ts
+++ src/components/teaser/teaser-tekster.ts
@@ -5,8 +5,8 @@ const TeaserTekster = {
     'spinnsyn.teaser.leste': 'Tidligere',
     'spinnsyn.sendt.til.nav': 'Sendt til NAV',
     'spinnsyn.sendt.til.arbeidsgiver': 'Sendt til arbeidsgiver',
-    'spinnsyn.teaser.annullert': 'Behandles på nytt',
-    'spinnsyn.teaser.annullert.tittel': 'Søknaden behandles på nytt'
+    'spinnsyn.teaser.annullert': 'Behandlet på nytt',
+    'spinnsyn.teaser.annullert.tittel': 'Søknaden er behandlet på nytt'
 }
 
 export default TeaserTekster
