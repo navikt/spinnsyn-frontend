43c3c52d1fdfd37dd6dbb103ca67ecf387a417d3
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2021-06-09 10:25:22 +0200

    Endre annullert tekster (#84)

--- src/pages/vedtak-side/annullering/annullering-tekster.ts
+++ src/pages/vedtak-side/annullering/annullering-tekster.ts
@@ -1,5 +1,5 @@
 const AnnulleringTekster = {
-    'annullering.alert': 'Vi har mottatt nye opplysninger som gjør at søknaden må behandles på nytt.',
+    'annullering.alert': 'Vi har mottatt nye opplysninger som gjør at søknaden er behandlet på nytt.',
     'annullering.info.header': 'Dette lurer mange på når søknaden behandles på nytt',
     'annullering.info.body': '<p><b>Hvorfor behandles den på nytt?</b><br />Når det kommer nye opplysninger i en sak, er det alltid slik at en saksbehandler ser på den. Dette er en generell regel.</p><p><b>Vil dette ha noe å si for pengene jeg får?</b><br />Du får sykepenger fra arbeidsgiveren din. Den nye behandlingen kan påvirke hva NAV betaler tilbake til arbeidsgiveren. Du vil høre fra arbeidsgiveren din hvis det er noen endringer.</p><p><b>Hvem har sendt opplysningene?\n</b><br />Vi får opplysninger enten fra deg, arbeidsgiveren din eller den som har sykmeldt deg.</p><p><b>Må jeg gjøre noe nå?</b><br />Du trenger ikke gjøre noe. Hvis vi trenger flere opplysninger, tar en av våre saksbehandlere kontakt med deg.</p>',
     'annullering.info.skriv-til-oss': 'Skriv til oss om du har flere spørsmål',
