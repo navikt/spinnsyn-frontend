f15f17eb1431d671de84e4fc92aa808e36a41632
Kjell Arne Brødreskift <kjell.arne.brodreskift@nav.no>
2021-07-15 13:19:15 +0200

    Lagt til id for aria-labelledby på teasere, og korrigert overskriftsnivå for riktig outline.

--- src/components/teaser/teaser-tekster.ts
+++ src/components/teaser/teaser-tekster.ts
@@ -1,8 +1,8 @@
 const TeaserTekster = {
     'spinnsyn.teaser.tittel': 'Behandlet søknad om sykepenger',
     'spinnsyn.teaser.sykmeldt-fra': 'Sykmeldt fra %ARBEIDSGIVER%',
-    'spinnsyn.teaser.uleste': 'Nye',
-    'spinnsyn.teaser.leste': 'Tidligere',
+    'spinnsyn.teaser.uleste': 'Nye søknader',
+    'spinnsyn.teaser.leste': 'Tidligere søknader',
     'spinnsyn.sendt.til.nav': 'Sendt til NAV',
     'spinnsyn.sendt.til.arbeidsgiver': 'Sendt til arbeidsgiver',
     'spinnsyn.teaser.annullert': 'Behandlet på nytt',
