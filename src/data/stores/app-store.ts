12d6c134a082acc46e7aba7e5bb1957a797823c3
Kjell Arne Brødreskift <kjell.arne.brodreskift@nav.no>
2021-07-23 08:52:20 +0200

    Oppgradert alle avhengigheter og fjernet de som ikke er våre.

--- src/data/stores/app-store.ts
+++ src/data/stores/app-store.ts
@@ -1,19 +1,17 @@
 import constate from 'constate'
-import React, { useState } from 'react'
+import { useState } from 'react'
 
 import { RSVedtakWrapper } from '../../types/rs-types/rs-vedtak'
 import { Soknad } from '../../types/types'
 
 export const [ AppStoreProvider, useAppStore ] = constate(() => {
     const [ soknader, setSoknader ] = useState<Soknad[]>([])
-    const [ valgtSoknad, setValgtSoknad ] = useState<Soknad>()
     const [ rsVedtak, setRsVedtak ] = useState<RSVedtakWrapper[]>([])
     const [ valgtVedtak, setValgtVedtak ] = useState<RSVedtakWrapper>()
     const [ feilState, setFeilState ] = useState<boolean>(false)
 
     return {
         soknader, setSoknader,
-        valgtSoknad, setValgtSoknad,
         rsVedtak, setRsVedtak,
         valgtVedtak, setValgtVedtak,
         feilState, setFeilState,
