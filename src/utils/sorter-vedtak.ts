a0b156f4b7bee8ea284e109d3be432347be68cab
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2021-08-23 10:46:55 +0200

    Sorterer gjeldende først i lista

--- src/utils/sorter-vedtak.ts
+++ src/utils/sorter-vedtak.ts
@@ -5,5 +5,15 @@ import { RSVedtakWrapper } from '../types/rs-types/rs-vedtak'
 export const sorterEtterNyesteTom = (vedtak1: RSVedtakWrapper, vedtak2: RSVedtakWrapper) => {
     const tom1 = dayjs(vedtak1.vedtak.tom).unix()
     const tom2 = dayjs(vedtak2.vedtak.tom).unix()
-    return tom2 - tom1
+
+    const diff = tom2 - tom1
+    if (diff == 0) {
+        if (vedtak1.revurdert  || vedtak1.annullert) {
+            return 1
+        }
+        if (vedtak2.revurdert  || vedtak2.annullert) {
+            return -1
+        }
+    }
+    return diff
 }
