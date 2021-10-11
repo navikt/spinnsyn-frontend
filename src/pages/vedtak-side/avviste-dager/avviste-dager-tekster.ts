a059683a0f5170dbdcab581af4797b9f55e49cb3
Golshid Bahadorian <36138378+golshidb93@users.noreply.github.com>
2021-10-11 15:09:12 +0200

    Dev utbetales ikke av nav (#139)
    Bruker ordet "utbetales" isteden for "dekkes"

--- src/pages/vedtak-side/avviste-dager/avviste-dager-tekster.ts
+++ src/pages/vedtak-side/avviste-dager/avviste-dager-tekster.ts
@@ -1,5 +1,5 @@
 const AvvisteDagerTekster = {
-    'avviste.dager.dekkes.ikke': 'Dekkes ikke av NAV',
+    'avviste.dager.dekkes.ikke': 'Utbetales ikke av NAV',
     'avviste.dager.intro': 'Vi ser at du ikke har rett til sykepenger for én eller flere dagene i sykmeldingen. Nedenfor ser du dagene du ikke får utbetaling for, og hvorfor.',
 }
 
