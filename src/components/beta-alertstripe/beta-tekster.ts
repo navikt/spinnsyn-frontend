5c74f8dc5d1d08b1daf2543fc4e04fb14b732708
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2021-10-14 09:27:31 +0200

    Feil og mangler (#142)

--- src/components/beta-alertstripe/beta-tekster.ts
+++ src/components/beta-alertstripe/beta-tekster.ts
@@ -1,6 +1,6 @@
 const BetaTekster = {
     'vedtak.betatittel': 'Du er kommet til en side som er under utvikling.',
-    'vedtak.betatekst': 'Mangler og feil kan forekomme. Det varierer også hvem som får se denne informasjonen. ' +
+    'vedtak.betatekst': 'Det varierer hvem som får se denne informasjonen. ' +
         '<a href="https://www.nav.no/no/person/arbeid/sykmeldt-arbeidsavklaringspenger-og-yrkesskade/sykepenger/snart-far-du-se-sykepengene-dine-pa-nav.no" target="_blank">Les om hva du kan forvente.</a> '
 
 }
