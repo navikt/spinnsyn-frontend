3b53a34e9717f01d02dd6223f03d669b6ad6a316
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2021-10-20 16:06:10 +0200

    fixes

---
+++ src/pages/_app.tsx
@@ -0,0 +1,40 @@
+import '../style/global.css'
+import '../app.less'
+import '../pages-cra/vedtak-side/vedtak-side.less'
+import '../pages-cra/vedtak-side/utbetaling/utbetaling.less'
+import '../pages-cra/vedtak-side/utbetaling/inntekt-info/inntekt-info.less'
+import '../pages-cra/vedtak-side/uenig/uenig.less'
+import '../pages-cra/vedtak-side/sykepengedager/sykepengedager.less'
+import '../pages-cra/vedtak-side/lokale-lenker/lokale-lenker.less'
+import '../pages-cra/vedtak-side/behandling/behandling.less'
+import '../pages-cra/vedtak-side/avviste-dager/avviste-dager.less'
+import '../pages-cra/vedtak-side/annullering/annullering.less'
+import '../components/utvidbar/utvidbar.less'
+import '../components/teaser/saksbehandlingstid-og-utbetaling/saksbehandlingstid-og-utbetaling.less'
+import '../components/inngang/inngangspanel.less'
+import '../components/dager/dag-tabell.less'
+import '../components/brodsmuler/brodsmuler.less'
+import '../components/banner/banner.less'
+import '../components/vedtak-status/vedtak-status.less'
+
+import type { AppProps as NextAppProps } from 'next/app'
+import React, { PropsWithChildren } from 'react'
+import { DehydratedState } from 'react-query/hydration'
+
+interface AppProps extends Omit<NextAppProps, 'pageProps'> {
+    pageProps: PropsWithChildren<unknown> & {
+        dehydratedState: DehydratedState;
+    };
+}
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+
+
+    return (
+
+        <Component {...pageProps} />
+
+    )
+}
+
+export default MyApp
