5002c6297991e6941120cc3b32eb863b536e0d2f
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2021-10-20 16:06:10 +0200

    fixes

--- src/app.tsx
+++ src/app.tsx
@@ -5,7 +5,6 @@ import React from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { Route, Switch } from 'react-router-dom'
 
-import { Amplitude } from './components/amplitude/amplitudeProvider'
 import { HotjarTrigger } from './components/hotjar-trigger'
 import StoreProvider from './data/stores/store-provider'
 import RedirectTilOversikt from './pages-cra/feil/redirect-til-oversikt'
@@ -33,17 +32,15 @@ const App = (): any => {
     return (
         <StoreProvider>
             <QueryClientProvider client={queryClient}>
-                <Amplitude>
-                    <HotjarTrigger>
-                        <main id="maincontent" className="maincontent" role="main" tabIndex={-1}>
-                            <Switch>
-                                <Route exact={true} path="/" component={VedtakListe} />
-                                <Route path={'/vedtak/:id'} component={VedtakSide} />
-                                <Route path={'/vedtak/'} component={RedirectTilOversikt} />
-                            </Switch>
-                        </main>
-                    </HotjarTrigger>
-                </Amplitude>
+                <HotjarTrigger>
+                    <main id="maincontent" className="maincontent" role="main" tabIndex={-1}>
+                        <Switch>
+                            <Route exact={true} path="/" component={VedtakListe} />
+                            <Route path={'/vedtak/:id'} component={VedtakSide} />
+                            <Route path={'/vedtak/'} component={RedirectTilOversikt} />
+                        </Switch>
+                    </main>
+                </HotjarTrigger>
             </QueryClientProvider>
         </StoreProvider>
     )
