a20bdea5eeb12b8ada0293bd1f45a6184b44be34
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2021-10-26 13:13:41 +0200

    Småfiks

--- src/pages/api/logger.ts
+++ src/pages/api/logger.ts
@@ -6,9 +6,9 @@ import { logger } from '../../utils/logger'
 const handler = (req: NextApiRequest, res: NextApiResponse): void => {
     const { level, ts, ...rest }: pino.LogEvent = req.body
 
-    rest.messages.forEach((message) => {
-        const log = typeof message === 'string' ? { message } : message
-        logger[ level.label ]({ ...log, time: ts, isFrontend: true, x_userAgent: req.headers[ 'user-agent' ] })
+    rest.messages.forEach((msg) => {
+        const log = typeof msg === 'string' ? { msg } : msg
+        logger[ level.label ]({ ...log, isFrontend: true, x_userAgent: req.headers[ 'user-agent' ] })
     })
 
     res.status(200).json({ ok: 'ok' })
