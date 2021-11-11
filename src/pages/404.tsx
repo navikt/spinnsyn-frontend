61501f30d09a13f54326883b32a4659cb8fa8d10
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2021-11-11 15:56:27 +0100

    Redirect fra /

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,15 @@
 import { ContentContainer } from '@navikt/ds-react'
-import React from 'react'
+import React, { useEffect } from 'react'
+
 
 function NotFound(): JSX.Element | boolean {
+    useEffect(() => {
+        if (window.location.pathname === '/') {
+            window.location.pathname = '/syk/sykepenger'
+        }
+
+    }, [])
+
     return (
         <ContentContainer>
             <div>Fant ikke siden</div>
