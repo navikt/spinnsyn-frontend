b87b9f41694790c856a89a3981885c0d2beee085
Kjell Arne Brødreskift <kjell.arne.brodreskift@nav.no>
2021-11-17 13:33:51 +0100

    Lagt til arbeidsgivernavn i sidetittel

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,6 @@
 import { hentLoginUrl } from './fetch'
 import { logger } from './logger'
 
-
 export const redirectTilLoginHvis401 = (res: Response) => {
     if (res.status === 401) {
         logger.warn('Redirecter til login grunnet 401')
@@ -10,7 +9,6 @@ export const redirectTilLoginHvis401 = (res: Response) => {
     }
     return false
 }
-
 
 export const setBodyClass = (name: string) => {
     if (document.body.className !== '') {
@@ -22,4 +20,22 @@ export const setBodyClass = (name: string) => {
 export const camelCaseTilSetning = (str: string) => {
     const medSpace = str.replace(/([a-z0-9])([A-Z])/g, '$1 $2') // Add space between camel casing
     return medSpace.charAt(0).toUpperCase() + medSpace.slice(1).toLowerCase() // Capitalize the first letter
+}
+
+export const storFørste = (str: string) => {
+    let ord = ''
+    ord += str.substring(0, 1).toUpperCase()
+    ord += str.substring(1, str.length).toLowerCase()
+    return ord
+}
+
+export const storeTilStoreOgSmå = (str: string) => {
+    const strArr = str.split(' ')
+    let navn = ''
+    strArr.map(ord => {
+        ord.toLowerCase() !== 'as' && !ord.toLowerCase().includes('a/s')
+            ? navn += storFørste(ord) + ' '
+            : navn += ord
+    })
+    return navn
 }
