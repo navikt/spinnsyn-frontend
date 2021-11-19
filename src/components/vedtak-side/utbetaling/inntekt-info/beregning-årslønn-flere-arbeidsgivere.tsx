aadefcfc049e502b98a538ba1947609242a797a9
golshidbahadorian <golshid.bahadorian@nav.no>
2021-11-19 12:28:24 +0100

    Legger til andreArbeidsgivere feltet og tilpasser tester

--- src/components/vedtak-side/utbetaling/inntekt-info/beregning-årslønn-flere-arbeidsgivere.tsx
+++ src/components/vedtak-side/utbetaling/inntekt-info/beregning-årslønn-flere-arbeidsgivere.tsx
@@ -1,6 +1,7 @@
 import { Element, Normaltekst } from 'nav-frontend-typografi'
 import React from 'react'
 
+import { storeTilStoreOgSmå } from '../../../../utils/store-små'
 import { tekst } from '../../../../utils/tekster'
 import { formaterValuta } from '../../../../utils/valuta-utils'
 import { VedtakProps } from '../../vedtak'
@@ -18,13 +19,13 @@ const BeregningÅrslønnFlereArbeidsgivere = ({ vedtak }: VedtakProps) => {
                 <Normaltekst tag="span">{}</Normaltekst>
             </div>
             <ul style={{ marginInlineStart: -8, marginTop: 0.1 }}>
-                { Object.keys(alleÅrsLønn!).map((key) => (
-                    <li key={key}>
+                { Object.keys(alleÅrsLønn!).map((arbeidsgiverNavn) => (
+                    <li key={arbeidsgiverNavn}>
                         <div className="inntekt__info__linje">
                             <Element className={'inntekt__info__redusert'} tag="p">
-                                {key}
+                                {storeTilStoreOgSmå(arbeidsgiverNavn)}
                             </Element>
-                            <Normaltekst tag="span">{formaterValuta(alleÅrsLønn![key])}</Normaltekst>
+                            <Normaltekst tag="span">{formaterValuta(alleÅrsLønn![arbeidsgiverNavn])}</Normaltekst>
                         </div>
                     </li>
                 ))}
