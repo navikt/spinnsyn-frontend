a0a2f958aec053a16bce83fbdd9b0b20cf9368d1
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2021-11-24 13:27:23 +0100

    Litt wait

--- cypress/integration/les-ulest-vedtak.ts
+++ cypress/integration/les-ulest-vedtak.ts
@@ -67,7 +67,11 @@ describe('Tester at appen starter', () => {
     })
 
     it('Vi går tilbake til oversikten', () => {
+        // eslint-disable-next-line cypress/no-unnecessary-waiting
+        cy.wait(200)
         cy.get(':nth-child(3) > .lenke').contains('Svar på søknader').click()
+        // eslint-disable-next-line cypress/no-unnecessary-waiting
+        cy.wait(200)
     })
 
     it('Det er 1 uleste vedtak og 7 leste', () => {
