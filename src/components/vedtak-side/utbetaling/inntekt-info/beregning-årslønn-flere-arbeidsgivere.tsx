fd4c875cddd5141e6ab64d3d3a563724eead0f91
Kjell Arne Brødreskift <kjell.arne.brodreskift@nav.no>
2021-11-25 13:03:17 +0100

    Endret markup i komponenten.

--- src/components/vedtak-side/utbetaling/inntekt-info/beregning-årslønn-flere-arbeidsgivere.tsx
+++ src/components/vedtak-side/utbetaling/inntekt-info/beregning-årslønn-flere-arbeidsgivere.tsx
@@ -12,24 +12,22 @@ const BeregningÅrslønnFlereArbeidsgivere = ({ vedtak }: VedtakProps) => {
 
     return (
         <>
-            <div className="inntekt__info__linje">
-                <Element tag="h4">
+            <tr>
+                <Element tag="th">
                     {tekst('utbetaling.andre.arbeidsgivere.tittel')}
                 </Element>
-                <Normaltekst tag="span">{}</Normaltekst>
-            </div>
-            <ul style={{ marginInlineStart: -8, marginTop: 0.1 }}>
-                { Object.keys(alleÅrsLønn!).map((arbeidsgiverNavn) => (
-                    <li key={arbeidsgiverNavn}>
-                        <div className="inntekt__info__linje">
-                            <Element className="italic" tag="p">
-                                {storeTilStoreOgSmå(arbeidsgiverNavn)}
-                            </Element>
-                            <Normaltekst tag="span">{formaterValuta(alleÅrsLønn![arbeidsgiverNavn])}</Normaltekst>
-                        </div>
-                    </li>
-                ))}
-            </ul>
+                <Normaltekst tag="td">{}</Normaltekst>
+            </tr>
+            {Object.keys(alleÅrsLønn!).map((arbeidsgiverNavn) => (
+                <tr key={arbeidsgiverNavn}>
+                    <Element tag="th" className="arbgivernavn">
+                        {storeTilStoreOgSmå(arbeidsgiverNavn)}
+                    </Element>
+                    <Normaltekst tag="td">
+                        {formaterValuta(alleÅrsLønn![arbeidsgiverNavn])}
+                    </Normaltekst>
+                </tr>
+            ))}
         </>
     )
 }
--- src/components/vedtak-side/utbetaling/inntekt-info/inntekt-info.tsx
+++ src/components/vedtak-side/utbetaling/inntekt-info/inntekt-info.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 import { harFlereArbeidsgivere } from '../../../../utils/har-flere-arbeidsgivere'
 import { storeTilStoreOgSmå } from '../../../../utils/store-små'
-import { getLedetekst,tekst } from '../../../../utils/tekster'
+import { getLedetekst, tekst } from '../../../../utils/tekster'
 import { formaterValuta } from '../../../../utils/valuta-utils'
 import Vis from '../../../vis'
 import { VedtakProps } from '../../vedtak'
@@ -19,100 +19,110 @@ const InntektInfo = ({ vedtak }: VedtakProps) => {
         ? formaterValuta(vedtak.vedtak.inntekt * 12)
         : undefined
 
-    const skalViseDagsats = vedtak.vedtak.sykepengegrunnlag && harFlereArbeidsgivere(vedtak) == 'nei'
+    const skalViseDagsats = vedtak.vedtak.sykepengegrunnlag && harFlereArbeidsgivere(vedtak) === 'nei'
     const dagsats = vedtak.vedtak.sykepengegrunnlag && (vedtak.vedtak.sykepengegrunnlag / 260) || 0
     const skalViseSykepengegrunnlag = vedtak.vedtak.sykepengegrunnlag
 
     return (
         <Vis hvis={inntektMnd && inntektAr}
             render={() =>
-                <section className="inntekt__info">
-
-                    <div className="inntekt__info__linje">
-                        <Element tag="h4">
-                            {getLedetekst(tekst('utbetaling.inntekt.info.beregnet'), {
-                                '%ARBEIDSGIVER%': storeTilStoreOgSmå(vedtak.orgnavn)
-                            })}
-                        </Element>
-                        <Normaltekst tag="span">{inntektMnd}</Normaltekst>
-                    </div>
-
-                    <div className="inntekt__info__linje">
-                        <Element tag="h4">
-                            {getLedetekst(tekst('utbetaling.inntekt.info.omregnet'), {
-                                '%ARBEIDSGIVER%': storeTilStoreOgSmå(vedtak.orgnavn)
-                            })}
-                        </Element>
-                        <Normaltekst tag="span">{inntektAr}</Normaltekst>
-                    </div>
-
-                    <Vis hvis={harFlereArbeidsgivere(vedtak) == 'ja'}
-                        render={() =>
-                            <>
-                                <BeregningÅrslønnFlereArbeidsgivere vedtak={vedtak} />
-                                <div className="inntekt__info__linje">
-                                    <Element tag="h4">
-                                        {tekst('utbetaling.inntekt.samlet.årslønn')}
+                <>
+                    <section className="inntekt__info">
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <Element tag="th">
+                                        {tekst('utbetaling.inntekt.info.beregnet')}
                                     </Element>
-                                    <Normaltekst tag="span">
-                                        {formaterValuta(vedtak.vedtak.grunnlagForSykepengegrunnlag!)}
-                                    </Normaltekst>
-                                </div>
-                            </>
-                        }
-                    />
+                                    <Normaltekst tag="td">{inntektMnd}</Normaltekst>
+                                </tr>
 
-                    <Vis hvis={skalViseSykepengegrunnlag}
-                        render={() =>
-                            <>
-                                <Vis hvis={vedtak.vedtak.begrensning == 'ER_6G_BEGRENSET'} render={() =>
-                                    <div className="inntekt__info__redusert_sykepengegrunnlag">
-                                        <img alt="" src={'/syk/sykepenger/static/img/info.svg'} />
-                                        <Element tag="span">
-                                            Redusert til 6G
-                                        </Element>
+                                <tr>
+                                    <Element tag="th">
+                                        {tekst('utbetaling.inntekt.info.omregnet')}
+                                    </Element>
+                                    <Normaltekst tag="td">{inntektAr}</Normaltekst>
+                                </tr>
+                            </tbody>
+                        </table>
 
-                                        <Normaltekst tag="p">
-                                            {tekst('utbetaling.redusert.til.6G')}
-                                        </Normaltekst>
-
-                                        <div className="inntekt__info__linje">
-                                            <Element tag="h4">
-                                                {tekst('utbetaling.sykepengegrunnlag')}
+                        <Vis hvis={harFlereArbeidsgivere(vedtak) === 'ja'}
+                            render={() =>
+                                <table className="flere-arbeidsgivere">
+                                    <tbody>
+                                        <BeregningÅrslønnFlereArbeidsgivere vedtak={vedtak} />
+                                        <tr>
+                                            <Element tag="th">
+                                                {tekst('utbetaling.inntekt.samlet.årslønn')}
                                             </Element>
-                                            <Normaltekst tag="span">
-                                                {formaterValuta(vedtak.vedtak.sykepengegrunnlag!)}
+                                            <Normaltekst tag="td">
+                                                {formaterValuta(vedtak.vedtak.grunnlagForSykepengegrunnlag!)}
                                             </Normaltekst>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            }
+                        />
+
+                        <Vis hvis={skalViseSykepengegrunnlag}
+                            render={() =>
+                                <>
+                                    <Vis hvis={vedtak.vedtak.begrensning === 'ER_6G_BEGRENSET'} render={() =>
+                                        <div className="redusert_sykepengegrunnlag">
+                                            <Element tag="div" className="img-rad">
+                                                <img alt="" src={'/syk/sykepenger/static/img/info.svg'} />
+                                                Redusert til 6G
+                                            </Element>
+                                            <Normaltekst>
+                                                {tekst('utbetaling.redusert.til.6G')}
+                                            </Normaltekst>
+                                            <table>
+                                                <tbody>
+                                                    <tr>
+                                                        <Element tag="th">
+                                                            {tekst('utbetaling.sykepengegrunnlag')}
+                                                        </Element>
+                                                        <Normaltekst tag="td">
+                                                            {formaterValuta(vedtak.vedtak.sykepengegrunnlag!)}
+                                                        </Normaltekst>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
                                         </div>
-                                    </div>
-                                } />
+                                    } />
 
-                                <Vis hvis={vedtak.vedtak.begrensning != 'ER_6G_BEGRENSET'} render={() =>
-                                    <div className="inntekt__info__linje">
-                                        <Element tag="h4">
-                                            {tekst('utbetaling.sykepengegrunnlag')}
-                                        </Element>
-                                        <Normaltekst tag="span">
-                                            {formaterValuta(vedtak.vedtak.sykepengegrunnlag!)}
-                                        </Normaltekst>
-                                    </div>
-                                } />
-                            </>
-                        }
-                    />
+                                    <Vis hvis={vedtak.vedtak.begrensning !== 'ER_6G_BEGRENSET'} render={() =>
+                                        <table>
+                                            <tbody>
+                                                <tr>
+                                                    <Element tag="th">
+                                                        {tekst('utbetaling.sykepengegrunnlag')}
+                                                    </Element>
+                                                    <Normaltekst tag="td">
+                                                        {formaterValuta(vedtak.vedtak.sykepengegrunnlag!)}
+                                                    </Normaltekst>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    } />
+                                </>
+                            }
+                        />
 
-                    <Vis hvis={skalViseDagsats}
-                        render={() =>
-                            <div className="inntekt__info__linje">
-                                <Element tag="h4">
-                                    {tekst('utbetaling.inntekt.info.dagsats')}
-                                </Element>
-                                <Normaltekst tag="span">{formaterValuta(dagsats)}</Normaltekst>
-                            </div>
-                        }
-                    />
-
-                </section>
+                        <Vis hvis={skalViseDagsats}
+                            render={() =>
+                                <table className="dagsats">
+                                    <tbody>
+                                        <tr>
+                                            <Element tag="th">{tekst('utbetaling.inntekt.info.dagsats')}</Element>
+                                            <Normaltekst tag="td">{formaterValuta(dagsats)}</Normaltekst>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            }
+                        />
+                    </section>
+                </>
             }
         />
     )
