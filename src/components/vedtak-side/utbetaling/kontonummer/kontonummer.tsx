7064476cc0d7a88c1ca794e2d6b6f39926c01e4a
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2021-12-01 13:42:50 +0100

    Hent faktisk kontonummer eller mockdata

--- src/components/vedtak-side/utbetaling/kontonummer/kontonummer.tsx
+++ src/components/vedtak-side/utbetaling/kontonummer/kontonummer.tsx
@@ -1,24 +1,48 @@
 import parser from 'html-react-parser'
 import { Element, Normaltekst } from 'nav-frontend-typografi'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
+import { Personalia } from '../../../../types/types'
+import { isMockBackend, isProd } from '../../../../utils/environment'
 import { tekst } from '../../../../utils/tekster'
 import Vis from '../../../vis'
 
 const Kontonummer = () => {
-    const url = new URL(window.location.href)
-    const testperson = url.searchParams.get('testperson')
-    const visKontonummer = (testperson === 'kun-direkte' || testperson === 'kombinasjon')
+    const [ kontonummer, setKontonummer ] = useState<string>()
+
+    useEffect(() => {
+        hentKontonummer()
+    }, [])
+
+    async function hentKontonummer() {
+        if (isProd()) {
+            // Hardkoder URL da dette er data som kun finnes i produksjon.
+            const res = await fetch('https://www.nav.no/person/personopplysninger-api/personalia', {
+                method: 'GET',
+                credentials: 'include',
+                headers: { 'Content-Type': 'application/json' }
+            })
+            const data: Personalia = await res.json()
+            setKontonummer(data?.personalia?.kontonr)
+        }
+
+        if (isMockBackend()) {
+            setKontonummer('10010110001')
+        }
+    }
+
+    const formaterKontonummer = (kontonummer: string) =>
+        kontonummer.replace(/^(.{4})(.{2})(.*)$/, '$1 $2 $3')
 
     return (
         <>
-            <Vis hvis={visKontonummer} render={() =>
+            <Vis hvis={kontonummer} render={() =>
                 <Normaltekst>
-                    <strong>{tekst('utbetaling.kontonummer.utbetales')}</strong> 9901 01 12345
+                    <strong>{tekst('utbetaling.kontonummer.utbetales')}</strong> {formaterKontonummer(kontonummer!)}
                 </Normaltekst>
             } />
 
-            <Vis hvis={testperson === 'kun-direkte-uten-kontonummer'} render={() =>
+            <Vis hvis={!kontonummer} render={() =>
                 <>
                     <Element tag="h2" className="info__tittel">
                         {tekst('utbetaling.kontonummer.tittel')}
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,2 +1,8 @@
 
+export interface Personalia {
+    personalia: {
+        kontonr: string
+    }
+}
+
 export type JaNeiVetIkke = 'ja' | 'nei' | 'vet_ikke';
