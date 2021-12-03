1d52a43c9b5c940d5baed8dd6bd23a983e05c476
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2021-12-03 13:06:31 +0100

    En enkel redirect

---
+++ src/pages/vedtak/[id].tsx
@@ -0,0 +1,19 @@
+import { useRouter } from 'next/router'
+import React, { useEffect } from 'react'
+
+const VedtakPage = () => {
+    const router = useRouter()
+    const { id } = router.query
+
+    useEffect(() => {
+        router.push('/?id=' + id)
+    }, [ id, router ])
+
+
+    return (
+        <strong>Vidersender..</strong>
+    )
+}
+
+
+export default VedtakPage
