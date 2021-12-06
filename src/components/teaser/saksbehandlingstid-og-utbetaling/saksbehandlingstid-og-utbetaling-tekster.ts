1e6a799c038ec22b65884e9b3ebca916a63de0ab
kulsveen <bjorn.inge.kulsveen@nav.no>
2021-12-06 08:55:07 +0100

    Fjerner panel om saksbehandlingstid i fra listevisningen

--- src/components/teaser/saksbehandlingstid-og-utbetaling/saksbehandlingstid-og-utbetaling-tekster.ts
+++
@@ -1,7 +0,0 @@
-const SaksbehandlingstidOgUtbetalingTekster = {
-    'saksbehandlingstid.utbetaling.tittel': 'Om saksbehandlingstid og utbetaling',
-    'saksbehandlingstid.utbetaling.lenke.url': 'https://www.nav.no/no/nav-og-samfunn/kontakt-nav/utbetalinger/utbetalinger/utbetalingsdatoer-feriepenger-og-skattetrekk/sykepenger_kap'
-}
-
-
-export default SaksbehandlingstidOgUtbetalingTekster
