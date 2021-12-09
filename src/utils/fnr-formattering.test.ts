b8b4d6ec330160f505c6883ae0ce0cc451611958
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2021-12-09 09:13:37 +0100

    Fikser formattering av fnr

---
+++ src/utils/fnr-formattering.test.ts
@@ -0,0 +1,15 @@
+import { expect } from '@jest/globals'
+
+import { vedtakMed40Grad } from '../data/mock/data/rs-vedtak'
+import { fnrForVisning } from './fnr-for-visning'
+import { harFlereArbeidsgivere } from './har-flere-arbeidsgivere'
+import { jsonDeepCopy } from './json-deep-copy'
+
+
+describe('Tester formattering av fnr', () => {
+
+    it('Formatteres riktig', () => {
+        expect(fnrForVisning('13068712345')).toEqual('130687 12345')
+    })
+
+})
