181c1c7d53b653ca317c62fbf692d2e8c2becb35
Kjell Arne Brødreskift <kjell.arne.brodreskift@nav.no>
2022-02-04 10:50:22 +0100

    Fjernet semikolon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -29,7 +29,7 @@ import { DehydratedState, Hydrate, QueryClient, QueryClientProvider } from 'reac
 interface AppProps extends Omit<NextAppProps, 'pageProps'> {
     pageProps: PropsWithChildren<unknown> & {
         dehydratedState: DehydratedState;
-    };
+    }
 }
 
 dayjs.locale({
