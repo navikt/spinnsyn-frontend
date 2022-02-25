b8def39af1004563f3f3a99149a5afc44e36c7c9
Kjell Arne Brødreskift <kjell.arne.brodreskift@nav.no>
2022-02-25 09:33:30 +0100

    Endret tekst i velkommen-boks.

--- src/components/vedtak-side/vedtak-tekster.ts
+++ src/components/vedtak-side/vedtak-tekster.ts
@@ -1,6 +1,6 @@
 const VedtakTekster = {
     'vedtak.sidetittel': 'Svar på søknad',
-    'vedtak.velkommen.tekst1': 'Svaret på søknaden om sykepenger er vedtaket i saken din. Nedenfor ser du hva vi har besluttet i saken din og hvorfor.',
+    'vedtak.velkommen.tekst1': 'Svaret på søknaden er vedtaket i saken din. Nedenfor ser du hva vi har besluttet i saken din og hvorfor.',
     'vedtak.velkommen.tekst2': 'Du får noen av sykepengene dine fra NAV og resten fra arbeidsgiveren din. Arbeidsgiveren din får igjen pengene fra NAV senere.',
     'vedtak.tilbake': 'Til hovedsiden ditt sykefravær',
 }
