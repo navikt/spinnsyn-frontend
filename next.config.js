839c3843ddaeb7afbfee37d3db1a009a488428e3
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-09-30 08:16:07 +0200

    Bump next

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const withLess = require('next-with-less')
 const { withSentryConfig } = require('@sentry/nextjs')
-const withPlugins = require('next-compose-plugins')
 
 const csp = {
     'default-src': ["'none'"],
@@ -43,79 +42,78 @@ const cspHeader = [
     },
 ]
 
-module.exports = withPlugins(
-    [
-        [withLess],
-        [
-            (nextConfig) =>
-                process.env.ENABLE_SENTRY
-                    ? withSentryConfig(nextConfig, {
-                          silent: true,
-                      })
-                    : nextConfig,
-        ],
-    ],
-    {
-        async headers() {
-            return [
-                {
-                    source: '/:path*',
-                    headers: cspHeader,
-                },
-                {
-                    source: '/api/:path*',
-                    headers: [
-                        {
-                            key: 'Cache-Control',
-                            value: 'private, no-cache, no-store, max-age=0, must-revalidate',
-                        },
-                    ],
-                },
-            ]
-        },
-        basePath: '/syk/sykepenger',
-        lessLoaderOptions: {},
-        pageExtensions: ['page.tsx', 'api.ts'],
-        assetPrefix: process.env.ASSET_PREFIX || '',
-        serverRuntimeConfig: {
-            // Will only be available on the server side
-            decoratorEnv: process.env.DECORATOR_ENV,
-            noDecorator: process.env.NO_DECORATOR,
-            utviklingArkivering: process.env.UTVIKLING_ARKIVERING,
-            arkivering: process.env.ARKIVERING,
-            spinnsynBackendUrl: process.env.SPINNSYN_BACKEND_URL,
-            spinnsynBackendTokenxClientId: process.env.SPINNSYN_BACKEND_TOKENX_CLIENT_ID,
-            sokosKontoregisterPersonTokenxClientId: process.env.SOKOS_KONTOREGISTER_PERSON_TOKENX_CLIENT_ID,
-            azureAppClientId: process.env.AZURE_APP_CLIENT_ID,
-            azureAppClientSecret: process.env.AZURE_APP_CLIENT_SECRET,
-            azureOpenidConfigTokenEndpoint: process.env.AZURE_OPENID_CONFIG_TOKEN_ENDPOINT,
-            azureAppWellKnownUrl: process.env.AZURE_APP_WELL_KNOWN_URL,
-            azureAppPreAuthorizedApps: process.env.AZURE_APP_PRE_AUTHORIZED_APPS,
-            spinnsynBackendClientId: process.env.SPINNSYN_BACKEND_CLIENT_ID,
-            flexFssProxyClientId: process.env.FLEX_FSS_PROXY_CLIENT_ID,
-            flexFssProxyUrl: process.env.FLEX_FSS_PROXY_URL,
-            idportenClientId: process.env.IDPORTEN_CLIENT_ID,
-            idportenWellKnownUrl: process.env.IDPORTEN_WELL_KNOWN_URL,
-            naisAppImage: process.env.NAIS_APP_IMAGE,
-            loginserviceUrl: process.env.LOGINSERVICE_URL,
-            loginServiceRedirectUrl: process.env.LOGINSERVICE_REDIRECT_URL,
-            loginserviceIdportenDiscoveryUrl: process.env.LOGINSERVICE_IDPORTEN_DISCOVERY_URL,
-            loginserviceIdportenAudience: process.env.LOGINSERVICE_IDPORTEN_AUDIENCE,
-            tokenXWellKnownUrl: process.env.TOKEN_X_WELL_KNOWN_URL,
-            tokenXPrivateJwk: process.env.TOKEN_X_PRIVATE_JWK,
-            tokenXClientId: process.env.TOKEN_X_CLIENT_ID,
-        },
-        publicRuntimeConfig: {
-            // Will be available on both server and client
-            flexGatewayRoot: process.env.FLEX_GATEWAY_ROOT,
-            mockBackend: process.env.MOCK_BACKEND,
-            opplaering: process.env.OPPLAERING,
-            sykefravaerUrl: process.env.SYKEFRAVAER_URL,
-            minSideUrl: process.env.MINSIDE_URL,
-            amplitudeEnabled: process.env.AMPLITUDE_ENABLED,
-            environment: process.env.ENVIRONMENT,
-            spinnsynFrontendInterne: process.env.SPINNSYN_FRONTEND_INTERNE,
-            arkiverteVedtakUrl: process.env.ARKIVERTE_VEDTAK_URL,
-        },
-    }
-)
+/**
+ * @type {import("next").NextConfig}
+ */
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: cspHeader,
+            },
+            {
+                source: '/api/:path*',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'private, no-cache, no-store, max-age=0, must-revalidate',
+                    },
+                ],
+            },
+        ]
+    },
+    basePath: '/syk/sykepenger',
+    lessLoaderOptions: {},
+    pageExtensions: ['page.tsx', 'api.ts'],
+    assetPrefix: process.env.ASSET_PREFIX || undefined,
+    serverRuntimeConfig: {
+        // Will only be available on the server side
+        decoratorEnv: process.env.DECORATOR_ENV,
+        noDecorator: process.env.NO_DECORATOR,
+        utviklingArkivering: process.env.UTVIKLING_ARKIVERING,
+        arkivering: process.env.ARKIVERING,
+        spinnsynBackendUrl: process.env.SPINNSYN_BACKEND_URL,
+        spinnsynBackendTokenxClientId: process.env.SPINNSYN_BACKEND_TOKENX_CLIENT_ID,
+        sokosKontoregisterPersonTokenxClientId: process.env.SOKOS_KONTOREGISTER_PERSON_TOKENX_CLIENT_ID,
+        azureAppClientId: process.env.AZURE_APP_CLIENT_ID,
+        azureAppClientSecret: process.env.AZURE_APP_CLIENT_SECRET,
+        azureOpenidConfigTokenEndpoint: process.env.AZURE_OPENID_CONFIG_TOKEN_ENDPOINT,
+        azureAppWellKnownUrl: process.env.AZURE_APP_WELL_KNOWN_URL,
+        azureAppPreAuthorizedApps: process.env.AZURE_APP_PRE_AUTHORIZED_APPS,
+        spinnsynBackendClientId: process.env.SPINNSYN_BACKEND_CLIENT_ID,
+        flexFssProxyClientId: process.env.FLEX_FSS_PROXY_CLIENT_ID,
+        flexFssProxyUrl: process.env.FLEX_FSS_PROXY_URL,
+        idportenClientId: process.env.IDPORTEN_CLIENT_ID,
+        idportenWellKnownUrl: process.env.IDPORTEN_WELL_KNOWN_URL,
+        naisAppImage: process.env.NAIS_APP_IMAGE,
+        loginserviceUrl: process.env.LOGINSERVICE_URL,
+        loginServiceRedirectUrl: process.env.LOGINSERVICE_REDIRECT_URL,
+        loginserviceIdportenDiscoveryUrl: process.env.LOGINSERVICE_IDPORTEN_DISCOVERY_URL,
+        loginserviceIdportenAudience: process.env.LOGINSERVICE_IDPORTEN_AUDIENCE,
+        tokenXWellKnownUrl: process.env.TOKEN_X_WELL_KNOWN_URL,
+        tokenXPrivateJwk: process.env.TOKEN_X_PRIVATE_JWK,
+        tokenXClientId: process.env.TOKEN_X_CLIENT_ID,
+    },
+    publicRuntimeConfig: {
+        // Will be available on both server and client
+        flexGatewayRoot: process.env.FLEX_GATEWAY_ROOT,
+        mockBackend: process.env.MOCK_BACKEND,
+        opplaering: process.env.OPPLAERING,
+        sykefravaerUrl: process.env.SYKEFRAVAER_URL,
+        minSideUrl: process.env.MINSIDE_URL,
+        amplitudeEnabled: process.env.AMPLITUDE_ENABLED,
+        environment: process.env.ENVIRONMENT,
+        spinnsynFrontendInterne: process.env.SPINNSYN_FRONTEND_INTERNE,
+        arkiverteVedtakUrl: process.env.ARKIVERTE_VEDTAK_URL,
+    },
+}
+
+const withSentry = (nextConfig) =>
+    process.env.ENABLE_SENTRY
+        ? withSentryConfig(nextConfig, {
+              silent: true,
+          })
+        : nextConfig
+
+module.exports = withLess(withSentry(nextConfig))
