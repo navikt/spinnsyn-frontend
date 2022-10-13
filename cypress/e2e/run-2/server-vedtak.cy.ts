6c27c83624878ea02d7b47abca9a637e5bb7f928
kulsveen <bjorn.inge.kulsveen@nav.no>
2022-10-13 13:11:06 +0200

    Må også endre navn på cypress axe regler

--- cypress/e2e/run-2/server-vedtak.cy.ts
+++ cypress/e2e/run-2/server-vedtak.cy.ts
@@ -2,7 +2,7 @@ describe('Server vedtak', () => {
     it('laster siden med alt ekspandert ', () => {
         cy.visit('http://localhost:8080/syk/sykepenger/vedtak/arkivering/utvikling-arkivering')
 
-        cy.contains('Svar på søknad')
+        cy.contains('Svar på søknad').and('is.visible')
         cy.contains('Du kan lese mer om hvordan sykepengene beregnes i')
         cy.contains(
             'Sykepenger betales bare for dagene mandag til fredag. Jobber du lørdager og søndager, blir disse dagene likevel regnet med i sykepengene du får. Inntekten som du har på helgedagene, blir fordelt på ukedagene.'
