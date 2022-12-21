94ae858df43c7406fbe6f9f22a7c05a97f7ab78f
Nils Jørgen Mittet <88632815+nilsjorgen@users.noreply.github.com>
2022-12-21 15:31:15 +0100

    Endre lenke til klageveilederen (#529)

--- src/components/vedtak-side/uenig/uenig-tekster.ts
+++ src/components/vedtak-side/uenig/uenig-tekster.ts
@@ -6,7 +6,7 @@ const UenigTekster = {
     'uenig.lenke1.url': 'https://www.nav.no/no/nav-og-samfunn/kontakt-nav/klage-ris-og-ros/klagerettigheter',
     'uenig.tekst3': ' eller gå rett til ',
     'uenig.lenke2': 'klageveilederen',
-    'uenig.lenke2.url': 'https://www.nav.no/soknader/nb/person/helse/sykepenger/NAV%2000-03.00/klage/brev',
+    'uenig.lenke2.url': 'https://klage.nav.no/nb/helse/sykepenger',
 }
 
 export default UenigTekster
