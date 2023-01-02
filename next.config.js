5cba66ac5525a539c547247c6294e8741bf7402b
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2023-01-02 14:31:13 +0100

    connect-src

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const { buildCspHeader } = require('@navikt/nav-dekoratoren-moduler/ssr')
 
 const appDirectives = {
     'script-src-elem': ['*.boost.ai'],
+    'connect-src': ['https://*.uxsignals.com'],
     'script-src': ['https://uxsignals-frontend.uxsignals.app.iterate.no'],
     'font-src': ['https://fonts.gstatic.com'],
 }
