f88d9a4167a76fdbbff0f3e6b13913cdcca79398
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2023-01-04 09:54:39 +0100

    Logg process.env.ENVIRONMENT

--- next.config.js
+++ next.config.js
@@ -14,6 +14,8 @@ const appDirectives = {
  */
 const nextConfig = {
     async headers() {
+        // eslint-disable-next-line no-console
+        console.log(`process.env.ENVIRONMENT: ${process.env.ENVIRONMENT}`)
         const env = { env: process.env.ENVIRONMENT }
         const csp = await buildCspHeader(appDirectives, env)
 
