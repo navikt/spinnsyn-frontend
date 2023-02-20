d9bf5553e3af1983a0133a04bfd7cdd6dc28faeb
kulsveen <bjorn.inge.kulsveen@nav.no>
2023-02-20 12:03:14 +0100

    Brukes i PersonutbetalingMedInntekt

--- src/components/vedtak-side/utbetaling/personutbetaling-med-inntekt.tsx
+++ src/components/vedtak-side/utbetaling/personutbetaling-med-inntekt.tsx
@@ -1,14 +1,14 @@
 import { Accordion, BodyShort, Heading } from '@navikt/ds-react'
-import React, { useContext, useState } from 'react'
+import React, { useContext } from 'react'
 
 import { ArkiveringContext } from '../../../context/arkivering-context'
 import { tekst } from '../../../utils/tekster'
 import { ValutaFormat } from '../../../utils/valuta-utils'
-import Ekspanderbar from '../../ekspanderbar/ekspanderbar'
 import Vis from '../../vis'
 import { VedtakProps } from '../vedtak'
 import VedtakPeriode from '../vedtak-periode/vedtak-periode'
 import { spinnsynFrontendInterne } from '../../../utils/environment'
+import UtbetalingPanel from '../../panel/utbetaling-panel'
 
 import BeregningInfo from './accordion/beregning-info'
 import { SykepengerTrekk } from './sykepenger-trekk'
@@ -18,43 +18,40 @@ import { SykepengerNar } from './accordion/sykepenger-nar'
 import { SykepengerPerDag } from './accordion/sykepenger-per-dag'
 
 export const PersonutbetalingMedInntekt = ({ vedtak }: VedtakProps) => {
-    const [apen] = useState<boolean>(false)
     const erArkivering = useContext(ArkiveringContext)
     const erInterne = spinnsynFrontendInterne()
 
     const belop = ValutaFormat.format(vedtak.sykepengebelopPerson)
 
     return (
-        <Ekspanderbar
-            type="gronn"
-            erUgyldig={vedtak.revurdert || vedtak.annullert}
-            ikon="/syk/sykepenger/static/img/ikon-ekspander-gronn.svg"
-            className="personutbetaling"
-            erApen={apen}
+        <UtbetalingPanel
             tittel={
-                <div className="ekspanderbar__tittel">
+                <>
                     <Heading level="2" size="large">
                         {belop + ' kroner'}
+                        <BodyShort as="span">{tekst('utbetaling.person.systemtittel')}</BodyShort>
                     </Heading>
-                    <BodyShort>{tekst('utbetaling.person.systemtittel')}</BodyShort>
-                </div>
+                </>
             }
+            erUgyldig={vedtak.revurdert || vedtak.annullert}
         >
-            <VedtakPeriode vedtak={vedtak} />
+            <>
+                <VedtakPeriode vedtak={vedtak} />
 
-            <SykepengerTrekk />
+                <SykepengerTrekk />
 
-            <Vis hvis={!erInterne && !erArkivering} render={() => <Kontonummer />} />
+                <Vis hvis={!erInterne && !erArkivering} render={() => <Kontonummer />} />
 
-            <Accordion>
-                <SykepengerNar />
+                <Accordion>
+                    <SykepengerNar />
 
-                <InntektInfo vedtak={vedtak} />
+                    <InntektInfo vedtak={vedtak} />
 
-                <SykepengerPerDag dager={vedtak.dagerPerson} />
+                    <SykepengerPerDag dager={vedtak.dagerPerson} />
 
-                <BeregningInfo vedtak={vedtak} mottaker={'person'} />
-            </Accordion>
-        </Ekspanderbar>
+                    <BeregningInfo vedtak={vedtak} mottaker={'person'} />
+                </Accordion>
+            </>
+        </UtbetalingPanel>
     )
 }
