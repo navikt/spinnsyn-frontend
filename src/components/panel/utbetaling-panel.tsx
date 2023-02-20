f95a73e40c1b10f6661c7edcafd23f7bf86a263f
kulsveen <bjorn.inge.kulsveen@nav.no>
2023-02-20 12:01:38 +0100

    Utbetaling panel

---
+++ src/components/panel/utbetaling-panel.tsx
@@ -0,0 +1,36 @@
+import { Panel } from '@navikt/ds-react'
+import React from 'react'
+
+import styles from './utbetaling-panel.module.css'
+
+interface UtbetalingPanelProps {
+    tittel: React.ReactNode
+    erUgyldig: boolean
+    children: React.ReactNode
+}
+
+const UtbetalingPanel = (props: UtbetalingPanelProps) => {
+    function erUgyldig() {
+        if (props.erUgyldig) {
+            return ` ${styles.ugyldig}`
+        }
+        return ''
+    }
+
+    return (
+        <Panel className={styles.panelWrapper + erUgyldig()}>
+            <div className={styles.heading}>
+                <img
+                    aria-hidden="true"
+                    src="/syk/sykepenger/static/img/ikon-ekspander-gronn.svg"
+                    alt=""
+                    className={styles.image}
+                />
+                <div>{props.tittel}</div>
+            </div>
+            <div className={styles.content}>{props.children}</div>
+        </Panel>
+    )
+}
+
+export default UtbetalingPanel
