9133a6337f16edb8ab294d2a5e50acaf8c28e433
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2023-03-24 10:51:59 +0100

    Bruk riktig casing for HTTP header

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -90,7 +90,7 @@ export const fetchJsonMedRequestId = async (url: string, options: RequestInit = 
             payload: await clonedResponse.text(),
             method: options.method || 'GET',
             responseCode: response.status,
-            contentLength: parseInt(response.headers.get('content-length') || '0'),
+            contentLength: parseInt(response.headers.get('Content-Length') || '0'),
         }
 
         try {
