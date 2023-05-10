602bc86337eea9964981f74b65cf802c1d4bdf66
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-05-10 14:24:08 +0200

    Revert "Disable i prod (#1008)"
    This reverts commit fdcf96aa463a87c39bea66b97ad9c4083c7c9c0a.

---
+++ cypress/e2e/run-2/inline-feedback.cy.ts
@@ -0,0 +1,44 @@
+import { kunDirekte } from '../../../src/data/testdata/data/rs-vedtak'
+
+describe('Inline feedback', () => {
+    const vedtak = kunDirekte
+
+    before(() => {
+        cy.visit('http://localhost:8080/syk/sykepenger?testperson=direkte-uten-kontonummer')
+    })
+
+    it('Laster vedtaket', () => {
+        cy.url().should('equal', 'http://localhost:8080/syk/sykepenger?testperson=direkte-uten-kontonummer')
+        cy.get(`a[href*=${vedtak.id}]`).click()
+    })
+
+    it('Kan gi ja feedback', () => {
+        cy.get('[data-cy="feedback-JA"]').contains('Ja').click()
+        cy.get('[data-cy="feedback-JA"]').should('have.css', 'background-color', 'rgb(38, 38, 38)')
+        cy.get('[data-cy="feedback-textarea"]').type('Dette er en test')
+
+        cy.get('[data-cy="send-feedback"]').contains('Send inn svar').click()
+        cy.contains('Takk for tilbakemeldingen!')
+    })
+
+    it('Kan gi nei feedback', () => {
+        cy.get('[data-cy="feedback-NEI"]').contains('Nei').click()
+        cy.get('[data-cy="feedback-NEI"]').should('have.css', 'background-color', 'rgb(38, 38, 38)')
+        cy.get('[data-cy="feedback-textarea"]').type('Dette er en test')
+
+        cy.get('[data-cy="send-feedback"]').contains('Send inn svar').click()
+        cy.contains('Takk for tilbakemeldingen!')
+    })
+
+    it('Kan gi forbedring feedback', () => {
+        cy.get('[data-cy="feedback-FORBEDRING"]').contains('Foreslå forbedring').click()
+        cy.get('[data-cy="feedback-FORBEDRING"]').should('have.css', 'background-color', 'rgb(38, 38, 38)')
+
+        cy.get('[data-cy="send-feedback"]').contains('Send inn svar').click()
+        cy.contains('Tilbakemeldingen kan ikke være tom. Legg til tekst i feltet.')
+        cy.get('[data-cy="feedback-textarea"]').type('Dette er en test')
+        cy.get('[data-cy="send-feedback"]').contains('Send inn svar').click()
+
+        cy.contains('Takk for tilbakemeldingen!')
+    })
+})
--- src/components/feedback/feedback.tsx
+++ src/components/feedback/feedback.tsx
@@ -3,7 +3,7 @@ import { MouseEvent, useContext, useEffect, useRef, useState } from 'react'
 
 import { cn } from '../../utils/tw-utils'
 import { ArkiveringContext } from '../../context/arkivering-context'
-import { isProd, spinnsynFrontendInterne } from '../../utils/environment'
+import { spinnsynFrontendInterne } from '../../utils/environment'
 import UseFlexjarFeedback from '../../hooks/useFlexjarFeedback'
 
 enum Feedbacktype {
@@ -47,7 +47,6 @@ export const Feedback = ({
         setErrorMsg(null)
     }, [activeState])
 
-    if (isProd()) return null
     if (arkivering || spinnsynFrontendInterne()) return null
     const fetchFeedback = async (): Promise<void> => {
         if (activeState === null) {
