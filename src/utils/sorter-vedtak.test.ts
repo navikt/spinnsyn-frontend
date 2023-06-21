27223b75cde92279a7aa5d2e29a96ac4921cd28c
golshidbahadorian <golshid.bahadorian@nav.no>
2023-06-21 07:59:53 +0200

    Sorterer listene i listevisningen etter nyeste fom

--- src/utils/sorter-vedtak.test.ts
+++ src/utils/sorter-vedtak.test.ts
@@ -3,23 +3,42 @@ import { expect } from '@jest/globals'
 import { RSVedtakWrapper } from '../types/rs-types/rs-vedtak'
 
 import { jsonDeepCopy } from './json-deep-copy'
-import { sorterEtterNyesteTom } from './sorter-vedtak'
+import { sorterEtterNyesteFom } from './sorter-vedtak'
 
+it('Sorterer vedtak med tidligere fom før vedtak med senere fom i lista', () => {
+    const eldre: RSVedtakWrapper = {
+        id: '1',
+        vedtak: { fom: '2020-06-13' },
+        revurdert: false,
+    } as any
+
+    const nyere = jsonDeepCopy(eldre)
+    nyere.vedtak.fom = '2020-06-14'
+
+    const nyeste = jsonDeepCopy(eldre)
+    nyeste.vedtak.fom = '2020-06-15'
+
+    const sorterteVedtak = [eldre, nyeste, nyere].sort(sorterEtterNyesteFom)
+
+    expect(sorterteVedtak[0]).toEqual(nyeste)
+    expect(sorterteVedtak[1]).toEqual(nyere)
+    expect(sorterteVedtak[2]).toEqual(eldre)
+})
 it('Sorterer gjeldende vedtak før revurderte i lista', () => {
     const gjeldende: RSVedtakWrapper = {
         id: '1',
-        vedtak: { tom: '2020-06-13' },
+        vedtak: { fom: '2020-06-13' },
         revurdert: false,
     } as any
     const revurdert = jsonDeepCopy(gjeldende)
     revurdert.revurdert = true
 
-    const sorterteVedtak = [gjeldende, revurdert].sort(sorterEtterNyesteTom)
+    const sorterteVedtak = [gjeldende, revurdert].sort(sorterEtterNyesteFom)
 
     expect(sorterteVedtak[0]).toEqual(gjeldende)
     expect(sorterteVedtak[1]).toEqual(revurdert)
 
-    const sorterteVedtakMotsattStart = [revurdert, gjeldende].sort(sorterEtterNyesteTom)
+    const sorterteVedtakMotsattStart = [revurdert, gjeldende].sort(sorterEtterNyesteFom)
 
     expect(sorterteVedtakMotsattStart[0]).toEqual(gjeldende)
     expect(sorterteVedtakMotsattStart[1]).toEqual(revurdert)
@@ -28,18 +47,18 @@ it('Sorterer gjeldende vedtak før revurderte i lista', () => {
 it('Sorterer gjeldende vedtak før annullerte i lista', () => {
     const gjeldende: RSVedtakWrapper = {
         id: '1',
-        vedtak: { tom: '2020-06-13' },
+        vedtak: { fom: '2020-06-13' },
         annullert: false,
     } as any
     const annullert = jsonDeepCopy(gjeldende)
     annullert.annullert = true
 
-    const sorterteVedtak = [gjeldende, annullert].sort(sorterEtterNyesteTom)
+    const sorterteVedtak = [gjeldende, annullert].sort(sorterEtterNyesteFom)
 
     expect(sorterteVedtak[0]).toEqual(gjeldende)
     expect(sorterteVedtak[1]).toEqual(annullert)
 
-    const sorterteVedtakMotsattStart = [annullert, gjeldende].sort(sorterEtterNyesteTom)
+    const sorterteVedtakMotsattStart = [annullert, gjeldende].sort(sorterEtterNyesteFom)
 
     expect(sorterteVedtakMotsattStart[0]).toEqual(gjeldende)
     expect(sorterteVedtakMotsattStart[1]).toEqual(annullert)
--- src/utils/sorter-vedtak.ts
+++ src/utils/sorter-vedtak.ts
@@ -17,3 +17,19 @@ export const sorterEtterNyesteTom = (vedtak1: RSVedtakWrapper, vedtak2: RSVedtak
     }
     return diff
 }
+
+export const sorterEtterNyesteFom = (vedtak1: RSVedtakWrapper, vedtak2: RSVedtakWrapper) => {
+    const tom1 = dayjs(vedtak1.vedtak.fom).unix()
+    const tom2 = dayjs(vedtak2.vedtak.fom).unix()
+
+    const diff = tom2 - tom1
+    if (diff == 0) {
+        if (vedtak1.revurdert || vedtak1.annullert) {
+            return 1
+        }
+        if (vedtak2.revurdert || vedtak2.annullert) {
+            return -1
+        }
+    }
+    return diff
+}
