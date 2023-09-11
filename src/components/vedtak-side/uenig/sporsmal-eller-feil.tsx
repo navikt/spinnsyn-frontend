9f171eeb89202889010e8f9b9f8037fffe03acee
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2023-09-11 11:21:05 +0200

    Nytt avsnitt spørsmål eller feil (#1273)
    * Nytt avsnitt spørsmål eller feil
    
    * amplitude
    
    * testfix

---
+++ src/components/vedtak-side/uenig/sporsmal-eller-feil.tsx
@@ -0,0 +1,32 @@
+import { BodyLong, Heading } from '@navikt/ds-react'
+import React from 'react'
+
+import { LenkeMedAmplitude } from '../../lenke/lenke-med-amplitude'
+import { VedtakProps } from '../vedtak'
+
+export const SporsmalEllerFeil = ({ vedtak }: VedtakProps) => {
+    const soknadsLenke = () => {
+        const relatertSoknad = vedtak.vedtak.dokumenter.filter((dokument) => dokument.type === 'Søknad')
+        if (relatertSoknad.length === 1) {
+            return {
+                url: '/syk/sykepengesoknad/sendt/' + relatertSoknad[0].dokumentId,
+                cleanUrl: '/syk/sykepengesoknad/sendt/soknadsid',
+            }
+        }
+        return { url: '/syk/sykepengesoknad' }
+    }
+
+    return (
+        <>
+            <Heading size="small" level="2">
+                Spørsmål eller feil
+            </Heading>
+            <BodyLong spacing>
+                {'Lurer du på noe eller har du funnet en feil i vedtaket, kan du '}
+                <LenkeMedAmplitude url="https://www.nav.no/kontaktoss" tekst="kontakte NAV" />.
+                {' Har du funnet en feil i vedtaket som skyldes feil i søknaden kan du endre dette selv ved å '}
+                <LenkeMedAmplitude {...soknadsLenke()} tekst="endre svarene i søknaden" />.
+            </BodyLong>
+        </>
+    )
+}
