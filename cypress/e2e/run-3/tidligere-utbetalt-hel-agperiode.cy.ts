e1dbb65b404adc7e48942f38d90a13c531f78b00
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-09-21 12:50:26 +0200

    Tekst over 16 dager

---
+++ cypress/e2e/run-3/tidligere-utbetalt-hel-agperiode.cy.ts
@@ -0,0 +1,24 @@
+describe('Tidligere utbetalt hel arbeidsgiverperiode', () => {
+    before(() => {
+        cy.visit('http://localhost:8080/syk/sykepenger?testperson=kun-direkte')
+        cy.findAllByRole('link', { name: /Sykmeldt fra /i }).should('have.length', 1)
+    })
+
+    it('Åpner vedtak', () => {
+        cy.findByRole('link', { name: /Sykmeldt fra /i }).click()
+        cy.contains('Svar på søknad om sykepenger').should('be.visible')
+    })
+
+    it('Åpner beregning av sykepengene', () => {
+        cy.get('body').findByRole('region', { name: 'Beregning av sykepengene' }).click()
+        cy.findByRole('region', { name: 'Beregning av sykepengene' }).within(() => {
+            cy.contains('Sykepenger per dag').should('be.visible')
+            cy.findByRole('button', { name: 'Sykepenger per dag' }).click()
+            cy.contains(
+                'Det er tidligere utbetalt en hel arbeidsgiverperiode. Etter dette har vi vurdert at du ikke har gjenopptatt arbeidet i mer enn 16 dager. NAV har derfor utbetalt sykepenger fra første dag du ble sykmeldt. ',
+            ).should('be.visible')
+        })
+    })
+})
+
+export {}
