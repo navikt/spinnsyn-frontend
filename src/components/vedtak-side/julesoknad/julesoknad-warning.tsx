f16175f817f481759ab298157fc0260fa7eb26d0
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-12-11 12:44:06 +0100

    tekst

--- src/components/vedtak-side/julesoknad/julesoknad-warning.tsx
+++ src/components/vedtak-side/julesoknad/julesoknad-warning.tsx
@@ -9,7 +9,7 @@ export const JulesoknadWarning = () => {
     }, [])
     return (
         <Alert variant="warning" className="mt-4">
-            For å sikre at så mange som mulig skal få utbetalt sykepenger før jul, gjør vi det for noen mulig å sende
+            For å sikre at så mange som mulig skal få utbetalt sykepenger før jul, gjør vi det mulig for noen å sende
             søknaden før sykmeldingsperioden er over. Vi gjør oppmerksom på at det kan bety at det kan gå noe tid fra
             utbetalingen i desember til neste utbetaling.
         </Alert>
