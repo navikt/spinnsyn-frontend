79dfc87e4fc2de5e227124fe2ce8f68c7b765947
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2024-01-05 07:41:31 +0100

    Ny skjønnsfastsatt infotekst (#1548)

--- src/components/vedtak-side/inntekter-lagt-til-grunn/inntekt-info-tekster.ts
+++ src/components/vedtak-side/inntekter-lagt-til-grunn/inntekt-info-tekster.ts
@@ -1,4 +1,4 @@
 export const inntektInfoTekster = {
     '25%avvik-skjønnsfastsatt':
-        'Siden det er mer enn 25% avvik mellom beregnet årsinntekt og innrapportert årsinntekt blir inntekten skjønnsfastsatt.',
+        'Det er mer enn 25% avvik mellom omregnet årsinntekt og rapportert årsinntekt fra A-ordningen. Årsinntekten din har derfor blitt skjønnsfastsatt.',
 }
