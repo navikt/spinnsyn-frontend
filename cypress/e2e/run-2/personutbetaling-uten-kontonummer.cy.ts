a4d6401458c0558bf53e1d40e04f5f5c94c51745
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2024-03-19 09:12:09 +0100

    Vis tittel over tekst om utbetaling

--- cypress/e2e/run-2/personutbetaling-uten-kontonummer.cy.ts
+++ cypress/e2e/run-2/personutbetaling-uten-kontonummer.cy.ts
@@ -15,13 +15,12 @@ describe('Personutbetaling uten kontonummer', () => {
 
     it('Viser info om at kontonummer mangler', () => {
         cy.get('[data-cy="header-sykepenger-til-deg"]').contains('24 550 kroner')
-
         cy.get('[data-cy="header-sykepenger-til-deg"]').contains('sykepenger til deg')
-
         cy.get('[data-cy*="personutbetaling"]').contains('Kontonummer for utbetaling')
-
         cy.get('[data-cy*="personutbetaling"]').contains(
             'Vi har ikke registrert noe kontonummer på deg, og anbefaler at du legger det inn på Min side slik at vi får utbetalt sykepengene til deg så raskt som mulig.',
         )
+
+        cy.contains('Pengene utbetales til deg')
     })
 })
