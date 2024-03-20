bb9bc79f7543cbc036b869b5f7b80468a731d1c8
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2024-03-20 14:50:52 +0100

    Bruk OASIS (#1731)
    * wip
    
    * Revert combo
    
    * OASIS wip
    
    * litt logging
    
    * Proxyer med url
    
    * Returnerer tokenet
    
    * fjerner logging
    
    * oasis
    
    * rebase
    
    * Gjenbruk getToken

--- src/auth/beskyttetApi.ts
+++ src/auth/beskyttetApi.ts
@@ -1,11 +1,10 @@
 import { logger } from '@navikt/next-logger'
 import { NextApiRequest, NextApiResponse } from 'next'
+import { getToken, validateIdportenToken } from '@navikt/oasis'
+import { validateAzureToken } from '@navikt/oasis/dist/validate'
 
 import metrics, { cleanPathForMetric } from '../metrics/metrics'
 import { isMockBackend, spinnsynFrontendInterne } from '../utils/environment'
-
-import { verifyAzureAccessTokenSpinnsynInterne } from './verifyAzureAccessTokenVedArkivering'
-import { verifyIdportenAccessToken } from './verifyIdportenAccessToken'
 
 type ApiHandler = (req: NextApiRequest, res: NextApiResponse) => void | Promise<void>
 
@@ -16,39 +15,31 @@ export function beskyttetApi(handler: ApiHandler): ApiHandler {
         if (isMockBackend()) {
             return handler(req, res)
         }
-
-        if (spinnsynFrontendInterne()) {
-            return beskyttetApiInterne(req, res)
-        }
-
         function send401() {
             metrics.apiUnauthorized.inc({ path: cleanPath }, 1)
-
             res.status(401).json({ message: 'Access denied' })
         }
 
-        async function beskyttetApiInterne(req: NextApiRequest, res: NextApiResponse) {
-            const bearerToken: string | null | undefined = req.headers['authorization']
-            if (!bearerToken) {
-                return send401()
-            }
-            try {
-                await verifyAzureAccessTokenSpinnsynInterne(bearerToken)
-            } catch (e) {
-                logger.error(e, 'Kunne ikke autentisere.')
+        const token = getToken(req)
+        if (!token) {
+            return send401()
+        }
+        if (spinnsynFrontendInterne()) {
+            return beskyttetApiInterne(req, res, token)
+        }
+
+        async function beskyttetApiInterne(req: NextApiRequest, res: NextApiResponse, token: string) {
+            const verified = await validateAzureToken(token)
+            if (!verified.ok) {
+                logger.warn('kunne ikke validere azuretoken i beskyttetApi')
                 return send401()
             }
             return handler(req, res)
         }
 
-        const bearerToken: string | null | undefined = req.headers['authorization']
-        if (!bearerToken) {
-            return send401()
-        }
-        try {
-            await verifyIdportenAccessToken(bearerToken)
-        } catch (e) {
-            logger.error(e, 'Kunne ikke validere idporten token i beskyttetApi.')
+        const result = await validateIdportenToken(token)
+        if (!result.ok) {
+            logger.warn('kunne ikke validere idportentoken i beskyttetApi')
             return send401()
         }
 
--- src/pages/api/spinnsyn-backend-veileder/vedtak.api.ts
+++ src/pages/api/spinnsyn-backend-veileder/vedtak.api.ts
@@ -1,34 +1,34 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import getConfig from 'next/config'
+import { getToken, validateAzureToken } from '@navikt/oasis'
+import { requestAzureOboToken } from '@navikt/oasis/dist/obo'
 
-import { hentVedtakFraSpinnsynBackendForInterne } from '../../../data/hentVedtakForInterne'
-import { verifyAzureAccessTokenSpinnsynInterne } from '../../../auth/verifyAzureAccessTokenVedArkivering'
 import { hentModiaContext } from '../../../data/hentModiaContext'
-import { getOboAccessToken } from '../../../auth/getOboAccessToken'
+import { hentVedtakFraSpinnsynBackendForInterne } from '../../../data/hentVedtakForInterne'
 
 const { serverRuntimeConfig } = getConfig()
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-    const bearerToken: string | null | undefined = req.headers['authorization']
-    if (!bearerToken) {
+    const accessToken = getToken(req)
+    if (!accessToken) {
         return res.status(401).json({ message: 'Mangler autorisasjonstoken.' })
     }
-    try {
-        await verifyAzureAccessTokenSpinnsynInterne(bearerToken)
-    } catch (e) {
+    const validert = await validateAzureToken(accessToken)
+    if (!validert.ok) {
         return res.status(401).json({ message: 'Feil i autorisasjonstoken.' })
     }
+
+    const spinnsynObo = await requestAzureOboToken(accessToken, serverRuntimeConfig.spinnsynBackendClientId)
+    if (!spinnsynObo.ok) {
+        return res.status(401).json({ message: 'Kunne ikke hente spinnsyn obo token' })
+    }
+
     const sykmeldtFnr = await hentModiaContext(req)
     if (!sykmeldtFnr) {
         return res.status(404).json({ message: 'Ingen person i modia' })
     }
 
-    const oboSpinnsynBackend = await getOboAccessToken(
-        req?.headers.authorization?.split(' ')[1],
-        serverRuntimeConfig.spinnsynBackendClientId,
-    )
-
-    const vedtekene = await hentVedtakFraSpinnsynBackendForInterne(oboSpinnsynBackend, sykmeldtFnr)
+    const vedtekene = await hentVedtakFraSpinnsynBackendForInterne(spinnsynObo.token, sykmeldtFnr)
     return res.status(200).json(vedtekene)
 }
 
