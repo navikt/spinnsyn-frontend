633c434069c4b578739538e1e35a140a7076ae68
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2024-11-06 16:14:41 +0100

    endrer på tekst

--- src/components/vedtak-side/avviste-dager/avviste-dager-tekster.ts
+++ src/components/vedtak-side/avviste-dager/avviste-dager-tekster.ts
@@ -1,7 +1,7 @@
 const AvvisteDagerTekster = {
     'avviste.dager.dekkes.ikke': 'Utbetales ikke av NAV',
     'avviste.dager.intro':
-        'Vi ser at du ikke har rett til sykepenger for én eller flere dagene i sykmeldingen. Nedenfor ser du dagene du ikke får utbetaling for, og hvorfor.',
+        'Vi ser at du ikke har rett til sykepenger for én eller flere av dagene i denne sykmeldingsperioden. Nedenfor ser du dagene du ikke får utbetaling for, og hvorfor.',
 }
 
 export default AvvisteDagerTekster
