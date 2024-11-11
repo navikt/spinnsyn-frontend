1a8faafac36a694c07e4e4b17d2a4c6d11f9f5a9
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2024-11-11 15:47:17 +0100

    Playwright (#2224)

---
+++ playwright/listevisng.spec.ts
@@ -0,0 +1,15 @@
+import { expect } from '@playwright/test'
+
+import { test } from './fixtures'
+
+test.describe('Tester visning av forside', () => {
+    test('Laster startside', async ({ page }) => {
+        await page.goto('http://localhost:3000/syk/sykepenger?id=a147e9a9-0aa2-4f5f-a8e3-c16c901e4071')
+
+        const header = page.locator('main').locator('h1').first()
+        await expect(header).toBeVisible()
+
+        await expect(header).toContainText('Svar på søknad om sykepenger')
+        await expect(page.locator('text=Pengene utbetales til arbeidsgiveren din\n')).toBeVisible()
+    })
+})
