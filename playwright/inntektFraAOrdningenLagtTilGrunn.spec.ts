0311ddbc071afd772747d51a30e0a789309dd3b1
golshidbahadorian <golshid.bahadorian@nav.no>
2024-11-26 13:54:10 +0100

    kilden til innhentet inntekt skal være responsive

--- playwright/inntektFraAOrdningenLagtTilGrunn.spec.ts
+++ playwright/inntektFraAOrdningenLagtTilGrunn.spec.ts
@@ -1,73 +1,103 @@
-import { expect } from '@playwright/test'
+import { test as base, expect } from '@playwright/test'
 
 import { inntektHentetFraAordningen } from '../src/data/testdata/data/vedtak/inntektHentetFraAordningen'
 import { formaterValuta } from '../src/utils/valuta-utils'
 
-import { test } from './fixtures'
+const configurations = [
+    { name: 'Desktop Chromium', options: { viewport: { width: 1920, height: 1080 } } },
+    { name: 'Mobile Chromium', options: { viewport: { width: 375, height: 667 }, isMobile: true } },
+    { name: 'Desktop Firefox', options: { viewport: { width: 1920, height: 1080 }, browserName: 'firefox' } },
+    {
+        name: 'Mobile Firefox',
+        options: { viewport: { width: 375, height: 667 }, isMobile: true, browserName: 'firefox' },
+    },
+    { name: 'Desktop WebKit', options: { viewport: { width: 1920, height: 1080 }, browserName: 'webkit' } },
+    {
+        name: 'Mobile WebKit',
+        options: { viewport: { width: 375, height: 667 }, isMobile: true, browserName: 'webkit' },
+    },
+]
 
-test.describe('Vedtak med inntekt fra a-ordningen lagt i grunn', () => {
-    const vedtak = inntektHentetFraAordningen
+configurations.forEach(({ name, options }) => {
+    const test = base.extend({
+        page: async ({ browser }, use) => {
+            const context = await browser.newContext(options)
+            const page = await context.newPage()
+            await use(page)
+            await context.close()
+        },
+    })
 
-    test('Sjekker informasjon relatert til inntekt fra a-ordningen', async ({ page }) => {
-        await page.goto('http://localhost:3000/syk/sykepenger?testperson=diverse-data')
-        await page.emulateMedia({ reducedMotion: 'reduce' })
-        await page.locator(`a[href*="${vedtak.id}"]`).click()
+    test.describe('Vedtak med inntekt fra a-ordningen lagt i grunn' + name, () => {
+        test('Sjekker informasjon relatert til inntekt fra a-ordningen', async ({ page }) => {
+            const vedtak = inntektHentetFraAordningen
 
-        const header = page.locator('main').locator('h1').first()
-        await expect(header).toBeVisible()
+            await page.goto('http://localhost:3000/syk/sykepenger?testperson=diverse-data')
+            await page.emulateMedia({ reducedMotion: 'reduce' })
+            await page.locator(`a[href*="${vedtak.id}"]`).click()
 
-        page.locator('text=3 021 kroner').locator('..').locator('text=til Posten Norge AS, Bærum')
+            const header = page.locator('main').locator('h1').first()
+            await expect(header).toBeVisible()
 
-        await page.locator('main').locator('role=region[name="Beregning av sykepengene"]').click()
+            const beregningArticle = page.locator('role=article[name="Beregning av sykepengene"]')
+            await page.locator('main').locator('role=region[name="Beregning av sykepengene"]').click()
 
-        const beregningArticle = page.locator('role=article[name="Beregning av sykepengene"]')
+            if (options.viewport.width === 1920) {
+                await expect(
+                    beregningArticle.locator('role=region[name="Beregnet månedsinntekt (hentet fra a-ordningen)"]'),
+                ).toContainText(formaterValuta(74675))
+            } else {
+                const beregnetMaanedsinntekt = beregningArticle.locator('.navds-body-short.navds-body-short--small', {
+                    hasText: 'Beregnet månedsinntekt',
+                })
+                await expect(beregnetMaanedsinntekt).toBeVisible()
 
-        const beregnetMaanedsinntekt = beregningArticle.locator('.navds-body-short.navds-body-short--small', {
-            hasText: 'Beregnet månedsinntekt',
+                const hentetFraAOrdningen = beregningArticle.locator('.navds-body-short.navds-body-short--small p')
+                await expect(hentetFraAOrdningen).toHaveText('(hentet fra a-ordningen)')
+
+                await expect(beregningArticle).toContainText(formaterValuta(74675))
+            }
+
+            await expect(beregningArticle.locator('role=region[name="Omregnet til årsinntekt"]')).toContainText(
+                formaterValuta(896100),
+            )
+
+            await page.locator('text=Mer om beregningen').click({ force: true })
+
+            await expect(
+                page.locator(
+                    'text=Nav bruker vanligvis gjennomsnittet av inntekten din fra de siste 3 månedene før du ble syk for å beregne sykepengene dine.',
+                ),
+            ).toBeVisible()
+
+            const link = page.locator('text=Her kan du lese mer om hvilke inntekter som tas med i beregningen.')
+            await expect(link).toBeVisible()
+            await expect(link).toHaveAttribute(
+                'href',
+                'https://www.nav.no/arbeidsgiver/inntektsmelding#manedsinntekten',
+            )
+
+            await expect(
+                page.locator(
+                    'text=Hvis du har flere arbeidsforhold, men ikke er sykmeldt fra alle, vil Nav beregne månedsinntekten din for de arbeidsforholdene du ikke er sykmeldt',
+                ),
+            ).toBeVisible()
+
+            const behandlingHeader = page.locator('[data-cy="behandling-header"]')
+            await expect(behandlingHeader).toHaveText('Søknaden er behandlet automatisk')
+            await expect(behandlingHeader).toBeVisible()
+
+            const behandlingBody = page.locator('[data-cy="behandling-body"]')
+            await expect(behandlingBody).toContainText(
+                'Vi fattet vedtaket 23. oktober 2021. Opplysningene er hentet fra søknaden din og offentlige registre.',
+            )
+            expect(
+                page
+                    .locator('text=Feil i vedtaket på grunn av feil i søknaden din?')
+                    .locator('..')
+                    .locator('text=Du kan endre dette selv ved å'),
+            )
+            expect(page.locator('text=Spørsmål til opplysninger hentet fra a-ordningen?'))
         })
-        await expect(beregnetMaanedsinntekt).toBeVisible()
-
-        const hentetFraAOrdningen = beregningArticle.locator('.navds-body-short.navds-body-short--small p')
-        await expect(hentetFraAOrdningen).toHaveText('(hentet fra a-ordningen)')
-
-        await expect(beregningArticle).toContainText(formaterValuta(74675))
-
-        await expect(beregningArticle.locator('role=region[name="Omregnet til årsinntekt"]')).toContainText(
-            formaterValuta(896100),
-        )
-
-        await page.locator('text=Mer om beregningen').click({ force: true })
-
-        await expect(
-            page.locator(
-                'text=Nav bruker vanligvis gjennomsnittet av inntekten din fra de siste 3 månedene før du ble syk for å beregne sykepengene dine.',
-            ),
-        ).toBeVisible()
-
-        const link = page.locator('text=Her kan du lese mer om hvilke inntekter som tas med i beregningen.')
-        await expect(link).toBeVisible()
-        await expect(link).toHaveAttribute('href', 'https://www.nav.no/arbeidsgiver/inntektsmelding#manedsinntekten')
-
-        await expect(
-            page.locator(
-                'text=Hvis du har flere arbeidsforhold, men ikke er sykmeldt fra alle, vil Nav beregne månedsinntekten din for de arbeidsforholdene du ikke er sykmeldt',
-            ),
-        ).toBeVisible()
-
-        const behandlingHeader = page.locator('[data-cy="behandling-header"]')
-        await expect(behandlingHeader).toHaveText('Søknaden er behandlet automatisk')
-        await expect(behandlingHeader).toBeVisible()
-
-        const behandlingBody = page.locator('[data-cy="behandling-body"]')
-        await expect(behandlingBody).toContainText(
-            'Vi fattet vedtaket 23. oktober 2021. Opplysningene er hentet fra søknaden din og offentlige registre.',
-        )
-        expect(
-            page
-                .locator('text=Feil i vedtaket på grunn av feil i søknaden din?')
-                .locator('..')
-                .locator('text=Du kan endre dette selv ved å'),
-        )
-        expect(page.locator('text=Spørsmål til opplysninger hentet fra a-ordningen?'))
     })
 })
