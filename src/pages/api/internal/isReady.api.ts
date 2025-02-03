4a33336caa77cf27e2e973aa8d9a800a63e72811
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2025-02-03 08:19:41 +0100

    Isalive sjekk mot idporten (#2380)

--- src/pages/api/internal/isReady.api.ts
+++ src/pages/api/internal/isReady.api.ts
@@ -1,11 +1,24 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+import { checkJwks } from '../../../utils/checkIdportenJwks'
+import { spinnsynFrontendArkivering, spinnsynFrontendInterne } from '../../../utils/environment'
+
 type Data = {
     message: string
 }
 
-const isReady = (req: NextApiRequest, res: NextApiResponse<Data>): void => {
-    res.status(200).json({ message: "I'm ready!" })
+const isReady = async (req: NextApiRequest, res: NextApiResponse<Data>): Promise<void> => {
+    if (spinnsynFrontendInterne() || spinnsynFrontendArkivering()) {
+        res.status(200).json({ message: "I'm ready!" })
+        return
+    }
+    const jwksOk = await checkJwks()
+
+    if (jwksOk) {
+        res.status(200).json({ message: "I'm ready!" })
+    } else {
+        res.status(500).json({ message: 'Not ready: JWKS check failed' })
+    }
 }
 
 export default isReady
