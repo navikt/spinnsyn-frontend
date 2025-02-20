60e65530c330674e74d44d43094014c658966ce4
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2025-02-20 15:16:37 +0100

    Knukket url fiks

--- src/components/vedtak-side/uenig/uenig-tekster.ts
+++ src/components/vedtak-side/uenig/uenig-tekster.ts
@@ -3,7 +3,7 @@ const UenigTekster = {
     'uenig.tekst1': 'Hvis du ikke er enig i resultatet, kan du klage innen %KLAGEFRIST%. ',
     'uenig.tekst2': 'Les mer om ',
     'uenig.lenke1': 'retten til å klage',
-    'uenig.lenke1.url': 'https://www.nav.no/no/nav-og-samfunn/kontakt-nav/klage-ris-og-ros/klagerettigheter',
+    'uenig.lenke1.url': 'https://www.nav.no/klagerettigheter',
     'uenig.tekst3': ' eller gå rett til ',
     'uenig.lenke2': 'klageveilederen',
     'uenig.lenke2.url': 'https://www.nav.no/klage#sykepenger',
