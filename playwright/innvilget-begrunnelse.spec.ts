24bf23346f8c8e6f32c32fb1b3213c16eb13ffc9
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2025-07-09 11:12:32 +0200

    Implementer automatisk UU-validering i Playwright-tester

--- playwright/innvilget-begrunnelse.spec.ts
+++ playwright/innvilget-begrunnelse.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test } from '@playwright/test'
+import { expect, test } from './fixtures'
 
 test.describe('Begrunnelse', () => {
     test('Vedtak med innvilget begrunnelse fra Bømlo', async ({ page }) => {
