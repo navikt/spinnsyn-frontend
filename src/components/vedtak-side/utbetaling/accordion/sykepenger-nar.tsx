1f58ecab21b833f4acf678f71b07da3e1441928f
Oivind Thorsen Kulsrud <oivind.thorsen.kulsrud@nav.no>
2025-09-16 11:27:58 +0200

    la til custom events

--- src/components/vedtak-side/utbetaling/accordion/sykepenger-nar.tsx
+++ src/components/vedtak-side/utbetaling/accordion/sykepenger-nar.tsx
@@ -1,16 +1,30 @@
 import { BodyLong, ReadMore } from '@navikt/ds-react'
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 
 import { tekst } from '../../../../utils/tekster'
 import { LenkeMedAmplitude } from '../../../lenke/lenke-med-amplitude'
 import { ArkiveringContext } from '../../../../context/arkivering-context'
 import { parserWithReplace } from '../../../../utils/html-react-parser-utils'
+import { logEvent } from '../../../amplitude/amplitude'
 
 export const SykepengerNar = () => {
     const isServer = useContext(ArkiveringContext)
 
+    const [expanded, setExpanded] = useState<boolean>(false)
+
     return (
-        <ReadMore defaultOpen={isServer} header={tekst('utbetaling.person.når')}>
+        <ReadMore
+            defaultOpen={isServer}
+            header={tekst('utbetaling.person.når')}
+            open={expanded}
+            onClick={() => {
+                logEvent(expanded ? 'readmore lukket' : 'readmore åpnet', {
+                    tittel: tekst('utbetaling.person.når'),
+                    component: 'SykepengerNar',
+                })
+                setExpanded((prev) => !prev)
+            }}
+        >
             <BodyLong>
                 {parserWithReplace(tekst('utbetaling.person.når.innhold'))}
 
