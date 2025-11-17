dfe979393a4a23b32e044a2fe9988597c6ad7142
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2025-11-17 16:40:55 +0100

    link til inntektsmeldingen i sporsmal-eller-feil

--- src/components/vedtak-side/uenig/sporsmal-eller-feil.tsx
+++ src/components/vedtak-side/uenig/sporsmal-eller-feil.tsx
@@ -2,35 +2,19 @@ import { BodyLong, Heading } from '@navikt/ds-react'
 import React from 'react'
 
 import { LenkeMedAmplitude } from '../../lenke/lenke-med-amplitude'
-import { VedtakProps } from '../vedtak'
+import { Dokument } from '../../../types/rs-types/rs-vedtak-felles'
 
-const renderInntektsopplysninger = (inntektFraAOrdningLagtTilGrunn: boolean) => {
-    if (inntektFraAOrdningLagtTilGrunn) {
-        return (
-            <>
-                <BodyLong spacing>
-                    Har du spørsmål om opplysningene som er hentet fra a-ordningen, kan du
-                    <LenkeMedAmplitude
-                        url="https://innboks.nav.no/s/skriv-til-oss?category=Helse"
-                        tekst="ta kontakt med Nav"
-                    />
-                    .
-                </BodyLong>
-            </>
-        )
-    } else {
-        return (
-            <>
-                <BodyLong spacing>
-                    Har du spørsmål til opplysningene i <LenkeMedAmplitude tekst="inntektsmeldingen" url="#" />, kontakt
-                    arbeidsgiveren din.
-                </BodyLong>
-            </>
-        )
+type SporsmalEllerFeilProps = {
+    vedtak: {
+        vedtak: {
+            dokumenter: Dokument[]
+            tags?: string[]
+            yrkesaktivitetstype: 'ARBEIDSTAKER' | 'SELVSTENDIG'
+        }
     }
 }
 
-export const SporsmalEllerFeil = ({ vedtak }: VedtakProps) => {
+export const SporsmalEllerFeil = ({ vedtak }: SporsmalEllerFeilProps) => {
     const soknadsLenke = () => {
         const relatertSoknad = vedtak.vedtak.dokumenter.filter((dokument) => dokument.type === 'Søknad')
         if (relatertSoknad.length === 1) {
@@ -40,6 +24,20 @@ export const SporsmalEllerFeil = ({ vedtak }: VedtakProps) => {
             }
         }
         return { url: '/syk/sykepengesoknad' }
+    }
+
+    const inntektsmeldingLenke = () => {
+        const relaterteInntektsmeldinger = vedtak.vedtak.dokumenter.filter(
+            (dokument) => dokument.type === 'Inntektsmelding',
+        )
+        if (relaterteInntektsmeldinger.length === 1) {
+            const inntektsmeldingId = relaterteInntektsmeldinger[0].dokumentId
+            return {
+                url: `/syk/sykefravaer/inntektsmeldinger/${inntektsmeldingId}`,
+                cleanUrl: '/syk/sykefravaer/inntektsmeldinger/inntektsmeldingId',
+            }
+        }
+        return { url: '/syk/sykefravaer/inntektsmeldinger' }
     }
 
     const inntektFraAOrdningLagtTilGrunn = vedtak.vedtak.tags?.includes('InntektFraAOrdningenLagtTilGrunn') || false
@@ -60,8 +58,45 @@ export const SporsmalEllerFeil = ({ vedtak }: VedtakProps) => {
                 Har du funnet en feil som skyldes feil i søknaden kan du{' '}
                 <LenkeMedAmplitude {...soknadsLenke()} tekst="endre svarene i søknaden" />.
             </BodyLong>
-            {vedtak.vedtak.yrkesaktivitetstype === 'ARBEIDSTAKER' &&
-                renderInntektsopplysninger(inntektFraAOrdningLagtTilGrunn)}
+            {vedtak.vedtak.yrkesaktivitetstype === 'ARBEIDSTAKER' && (
+                <Inntektsopplysninger
+                    inntektFraAOrdningLagtTilGrunn={inntektFraAOrdningLagtTilGrunn}
+                    inntektsmeldingLenke={inntektsmeldingLenke}
+                />
+            )}
         </>
     )
+}
+
+const Inntektsopplysninger = ({
+    inntektFraAOrdningLagtTilGrunn,
+    inntektsmeldingLenke,
+}: {
+    inntektFraAOrdningLagtTilGrunn: boolean
+    inntektsmeldingLenke: () => { url: string; cleanUrl?: string }
+}) => {
+    if (inntektFraAOrdningLagtTilGrunn) {
+        return (
+            <>
+                <BodyLong spacing>
+                    Har du spørsmål om opplysningene som er hentet fra a-ordningen, kan du
+                    <LenkeMedAmplitude
+                        url="https://innboks.nav.no/s/skriv-til-oss?category=Helse"
+                        tekst="ta kontakt med Nav"
+                    />
+                    .
+                </BodyLong>
+            </>
+        )
+    } else {
+        return (
+            <>
+                <BodyLong spacing>
+                    Har du spørsmål til opplysningene i{' '}
+                    <LenkeMedAmplitude {...inntektsmeldingLenke()} tekst="inntektsmeldingen" />, kontakt arbeidsgiveren
+                    din.
+                </BodyLong>
+            </>
+        )
+    }
 }
