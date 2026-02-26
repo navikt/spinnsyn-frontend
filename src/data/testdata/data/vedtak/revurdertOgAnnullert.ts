32d2f8e83b9665def33f6f20fb80c3ad2e95bef6
Steinar Sørli Hegna <steinar.sorli.hegna@nav.no>
2026-02-26 16:49:10 +0100

    Fiks demoscenarioer 2
    Co-authored-by: Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>

--- src/data/testdata/data/vedtak/revurdertOgAnnullert.ts
+++ src/data/testdata/data/vedtak/revurdertOgAnnullert.ts
@@ -6,5 +6,3 @@ export const revurdertOgAnnullertVedtak = jsonDeepCopy(vedtakAnnullert)
 revurdertOgAnnullertVedtak.id = '9ae82dd2-dcf1-4c16-9e12-35cb6d634326'
 revurdertOgAnnullertVedtak.revurdert = true
 revurdertOgAnnullertVedtak.annullert = true
-revurdertOgAnnullertVedtak.sykepengebelopArbeidsgiver = 0
-revurdertOgAnnullertVedtak.sykepengebelopSykmeldt = 0
