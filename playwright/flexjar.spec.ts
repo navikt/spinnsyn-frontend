6b6fad3cf38074ad37cfa4a77d0ccba350900dc3
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2026-04-27 15:39:16 +0200

    Revert "Uten flexjar"
    This reverts commit 4b75565446401f58b13aecd6471c40a23b2c0e79.

---
+++ playwright/flexjar.spec.ts
@@ -0,0 +1,70 @@
+import { kunDirekte } from '../src/data/testdata/data/vedtak/kunDirekte'
+
+import { test, expect } from './fixtures'
+import { harSynligTittel, trykkPaVedtakMedId } from './utils/hjelpefunksjoner'
+
+const baseUrl = '/syk/sykepenger?testperson=direkte-uten-kontonummer'
+const poHelseUrl =
+    '/syk/sykepenger?testperson=flexjar-pohelse&flexjar-spinnsyn-pohelse-helsemetrikk=true&flexjar-spinnsyn-frontend=false&id=348a5462-456a-4bfc-9b54-11cd77a9937f'
+
+test.describe('Flexjar', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto(baseUrl, { waitUntil: 'networkidle' })
+
+        await expect(page.getByRole('link', { name: /Sykmeldt fra /i })).toHaveCount(1)
+        await trykkPaVedtakMedId(page, kunDirekte.id)
+        await harSynligTittel(page, 'Svar på søknad om sykepenger', 1)
+    })
+
+    test('Kan gi positiv feedback', async ({ page }) => {
+        const flexjarHeading = await harSynligTittel(page, 'Vil du hjelpe oss å gjøre denne siden bedre?', 2)
+        const region = page.getByRole('region').filter({ has: flexjarHeading })
+        await region.getByRole('radio', { name: 'Veldig enkelt' }).check()
+        await expect(region.getByRole('radio', { name: 'Veldig enkelt' })).toBeChecked()
+
+        await test.step('Payload inneholder arbeidssituasjon', async () => {
+            const sendKnapp = region.getByRole('button', { name: 'Send tilbakemelding' })
+            await expect(sendKnapp).toBeEnabled()
+            const [request] = await Promise.all([
+                page.waitForRequest((request) => request.url().includes('/flexjar-backend/api/v2/feedback')),
+                sendKnapp.click(),
+            ])
+            const postData = JSON.parse(request.postData() || '{}')
+            expect(postData).toHaveProperty('arbeidssituasjon', 'ARBEIDSTAKER')
+        })
+
+        await expect(page.getByText('Takk for tilbakemeldingen!')).toBeVisible()
+    })
+
+    test('Kan gi negativ feedback', async ({ page }) => {
+        await page.reload()
+        const flexjarHeading = await harSynligTittel(page, 'Vil du hjelpe oss å gjøre denne siden bedre?', 2)
+        const region = page.getByRole('region').filter({ has: flexjarHeading })
+        await region.getByRole('radio', { name: 'Veldig vanskelig' }).check()
+        await expect(region.getByRole('radio', { name: 'Veldig vanskelig' })).toBeChecked()
+        await region.getByRole('radio', { name: 'Skjønte ikke hvorfor svaret ble som det ble' }).check()
+        await expect(region.getByRole('radio', { name: 'Skjønte ikke hvorfor svaret ble som det ble' })).toBeChecked()
+        await region.getByRole('textbox').fill('Dette er en test')
+        const sendKnapp = region.getByRole('button', { name: 'Send tilbakemelding' })
+        await expect(sendKnapp).toBeEnabled()
+        await sendKnapp.click()
+        await expect(page.getByText('Takk for tilbakemeldingen!')).toBeVisible()
+    })
+
+    test('Har flexjar når det er riktige toggles', async ({ page }) => {
+        await page.goto(poHelseUrl)
+        await expect(page.getByText('Hvordan opplevde du å søke og å få svar på søknaden om sykepenger?')).toBeVisible()
+        const flexjarHeading = await harSynligTittel(page, 'Hva synes du?', 2)
+        const region = page.getByRole('region').filter({ has: flexjarHeading })
+
+        const braKnapp = region.getByRole('button', { name: 'Bra', exact: true })
+        await expect(braKnapp).toHaveAttribute('aria-pressed', 'false')
+        await braKnapp.click()
+        await expect(braKnapp).toHaveAttribute('aria-pressed', 'true')
+        await region.getByRole('textbox').fill('Dette er en test')
+        const sendKnapp = region.getByRole('button', { name: 'Send tilbakemelding' })
+        await expect(sendKnapp).toBeEnabled()
+        await sendKnapp.click()
+        await expect(page.getByText('Takk for tilbakemeldingen!')).toBeVisible()
+    })
+})
